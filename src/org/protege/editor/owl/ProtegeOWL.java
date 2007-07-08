59cbc9da2846fadc2e3ded630d53f27142554fe9
Timothy Redmond <tredmond@stanford.edu>
2007-07-08 05:44:18 +0000

    first runnable equinox protege 4 owl editor

---
+++ src/org/protege/editor/owl/ProtegeOWL.java
@@ -0,0 +1,15 @@
+package org.protege.editor.owl;
+
+
+
+/**
+ * This class is minimal for the moment.  We can refactor or maybe more stuff (an
+ * Activator?) will come here.
+ * @author tredmond
+ *
+ */
+public class ProtegeOWL {
+
+    public static final String ID = "org.protege.editor.owl";
+
+}
