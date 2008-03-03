b4507919b0d5a2112d0e4690208ef6fa2953de1e
Luigi Iannone <iannone@cs.man.ac.uk>
2008-03-03 16:44:30 +0000

    20080303LuigiBug31

--- src/org/protege/editor/owl/ui/frame/OWLObjectPropertyIndividualPairEditor.java
+++ src/org/protege/editor/owl/ui/frame/OWLObjectPropertyIndividualPairEditor.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.ListSelectionModel;
 
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.selector.OWLIndividualSelectorPanel;
@@ -16,70 +17,69 @@ import org.semanticweb.owl.model.OWLObjectProperty;
 import org.semanticweb.owl.model.OWLObjectPropertyExpression;
 import org.semanticweb.owl.model.OWLPropertyAssertionAxiom;
 
-
 /**
  * Author: Matthew Horridge<br>
  * The University Of Manchester<br>
  * Bio-Health Informatics Group<br>
- * Date: 22-Feb-2007<br><br>
+ * Date: 22-Feb-2007<br>
+ * <br>
  */
-public class OWLObjectPropertyIndividualPairEditor extends AbstractOWLFrameSectionRowObjectEditor<OWLObjectPropertyIndividualPair> {
+public class OWLObjectPropertyIndividualPairEditor extends
+		AbstractOWLFrameSectionRowObjectEditor<OWLObjectPropertyIndividualPair> {
+	private JPanel editorPanel;
+	private OWLObjectPropertySelectorPanel objectPropertyPanel;
+	private OWLIndividualSelectorPanel individualSelectorPanel;
 
-    private JPanel editorPanel;
+	public OWLObjectPropertyIndividualPairEditor(OWLEditorKit owlEditorKit) {
+		this.editorPanel = new JPanel(new BorderLayout());
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		this.objectPropertyPanel = new OWLObjectPropertySelectorPanel(
+				owlEditorKit);
+		splitPane.setLeftComponent(this.objectPropertyPanel);
+		this.individualSelectorPanel = new OWLIndividualSelectorPanel(
+				owlEditorKit, ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		splitPane.setRightComponent(this.individualSelectorPanel);
+		this.editorPanel.add(splitPane);
+	}
 
-    private OWLObjectPropertySelectorPanel objectPropertyPanel;
+	public void setObjectPropertyAxiom(
+			OWLPropertyAssertionAxiom<OWLObjectPropertyExpression, OWLIndividual> ax) {
+		OWLObjectPropertyExpression p = ax.getProperty();
+		if (p instanceof OWLObjectProperty) {
+			this.objectPropertyPanel
+					.setSelectedOWLObjectProperty((OWLObjectProperty) p);
+		}
+		this.individualSelectorPanel.setSelectedIndividual(ax.getObject());
+	}
 
-    private OWLIndividualSelectorPanel individualSelectorPanel;
+	public void clear() {
+	}
 
+	public OWLObjectPropertyIndividualPair getEditedObject() {
+		return new OWLObjectPropertyIndividualPair(this.objectPropertyPanel
+				.getSelectedOWLObjectProperty(), this.individualSelectorPanel
+				.getSelectedIndividual());
+	}
 
-    public OWLObjectPropertyIndividualPairEditor(OWLEditorKit owlEditorKit) {
-        editorPanel = new JPanel(new BorderLayout());
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        objectPropertyPanel = new OWLObjectPropertySelectorPanel(owlEditorKit);
-        splitPane.setLeftComponent(objectPropertyPanel);
-        individualSelectorPanel = new OWLIndividualSelectorPanel(owlEditorKit);
-        splitPane.setRightComponent(individualSelectorPanel);
-        editorPanel.add(splitPane);
-    }
+	@Override
+	public Set<OWLObjectPropertyIndividualPair> getEditedObjects() {
+		Set<OWLObjectPropertyIndividualPair> pairs = new HashSet<OWLObjectPropertyIndividualPair>();
+		for (OWLObjectProperty prop : this.objectPropertyPanel
+				.getSelectedOWLObjectProperties()) {
+			for (OWLIndividual ind : this.individualSelectorPanel
+					.getSelectedIndividuals()) {
+				pairs.add(new OWLObjectPropertyIndividualPair(prop, ind));
+			}
+		}
+		return pairs;
+	}
 
+	public JComponent getEditorComponent() {
+		return this.editorPanel;
+	}
 
-    public void setObjectPropertyAxiom(OWLPropertyAssertionAxiom<OWLObjectPropertyExpression, OWLIndividual> ax) {
-        OWLObjectPropertyExpression p = ax.getProperty();
-        if (p instanceof OWLObjectProperty){
-            objectPropertyPanel.setSelectedOWLObjectProperty((OWLObjectProperty)p);
-        }
-        individualSelectorPanel.setSelectedIndividual(ax.getObject());
-    }
-
-
-    public void clear() {
-    }
-
-
-    public OWLObjectPropertyIndividualPair getEditedObject() {
-        return new OWLObjectPropertyIndividualPair(objectPropertyPanel.getSelectedOWLObjectProperty(),
-                                                   individualSelectorPanel.getSelectedIndividual());
-    }
-
-
-    public Set<OWLObjectPropertyIndividualPair> getEditedObjects() {
-        Set<OWLObjectPropertyIndividualPair> pairs = new HashSet<OWLObjectPropertyIndividualPair>();
-        for (OWLObjectProperty prop : objectPropertyPanel.getSelectedOWLObjectProperties()) {
-            for (OWLIndividual ind : individualSelectorPanel.getSelectedIndividuals()) {
-                pairs.add(new OWLObjectPropertyIndividualPair(prop, ind));
-            }
-        }
-        return pairs;
-    }
-
-
-    public JComponent getEditorComponent() {
-        return editorPanel;
-    }
-
-
-    public void dispose() {
-        objectPropertyPanel.dispose();
-        individualSelectorPanel.dispose();
-    }
+	public void dispose() {
+		this.objectPropertyPanel.dispose();
+		this.individualSelectorPanel.dispose();
+	}
 }
