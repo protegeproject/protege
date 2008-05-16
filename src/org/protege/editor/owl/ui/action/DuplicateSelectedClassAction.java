3a81ed442232d91c3a5ae9417170710736ae3899
Nick Drummond <nickdrummond@yahoo.com>
2008-05-16 13:47:44 +0000

    - bugfix #303 Duplicating a class does not duplicate its annotations - added a further option to let the user copy annotations as well - only annotations that are not used to render the entity are copied (to avoid name duplication)

--- src/org/protege/editor/owl/ui/action/DuplicateSelectedClassAction.java
+++ src/org/protege/editor/owl/ui/action/DuplicateSelectedClassAction.java
@@ -1,21 +1,22 @@
 package org.protege.editor.owl.ui.action;
 
+import org.apache.log4j.Logger;
+import org.protege.editor.owl.model.OWLModelManager;
+import org.protege.editor.owl.model.entity.OWLEntityCreationSet;
+import org.protege.editor.owl.ui.UIHelper;
+import org.protege.editor.owl.ui.renderer.OWLEntityAnnotationValueRenderer;
+import org.protege.editor.owl.ui.renderer.OWLModelManagerEntityRenderer;
+import org.protege.editor.owl.ui.renderer.OWLRendererPreferences;
+import org.semanticweb.owl.model.*;
+import org.semanticweb.owl.util.OWLObjectDuplicator;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.log4j.Logger;
-import org.protege.editor.owl.model.entity.OWLEntityCreationSet;
-import org.semanticweb.owl.model.AddAxiom;
-import org.semanticweb.owl.model.OWLAxiom;
-import org.semanticweb.owl.model.OWLClass;
-import org.semanticweb.owl.model.OWLDisjointClassesAxiom;
-import org.semanticweb.owl.model.OWLOntology;
-import org.semanticweb.owl.model.OWLOntologyChange;
-import org.semanticweb.owl.util.OWLObjectDuplicator;
 
 
 /**
@@ -39,11 +40,31 @@ public class DuplicateSelectedClassAction extends SelectedOWLClassAction {
     public void actionPerformed(ActionEvent e) {
         OWLClass selectedClass = getOWLWorkspace().getOWLSelectionModel().getLastSelectedClass();
         OWLEntityCreationSet<OWLClass> set = getOWLWorkspace().createOWLClass();
+
         Map<URI, URI> replacementURIMap = new HashMap<URI, URI>();
         replacementURIMap.put(selectedClass.getURI(), set.getOWLEntity().getURI());
-        OWLObjectDuplicator dup = new OWLObjectDuplicator(getOWLModelManager().getOWLDataFactory(), replacementURIMap);
+        OWLModelManager mngr = getOWLModelManager();
+        OWLObjectDuplicator dup = new OWLObjectDuplicator(mngr.getOWLDataFactory(), replacementURIMap);
         List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+
         changes.addAll(set.getOntologyChanges());
+
+        changes.addAll(duplicateClassAxioms(selectedClass, dup));
+
+        UIHelper uiHelper = new UIHelper(getOWLEditorKit());
+        if (uiHelper.showOptionPane("Duplicate Class",
+                "Would you like to duplicate annotations?",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+            changes.addAll(duplicateAnnotations(selectedClass, dup));
+        }
+
+        mngr.applyChanges(changes);
+        getOWLWorkspace().getOWLSelectionModel().setSelectedEntity(set.getOWLEntity());
+    }
+
+    private List<OWLOntologyChange> duplicateClassAxioms(OWLClass selectedClass, OWLObjectDuplicator dup) {
+        List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
         for (OWLOntology ont : getOWLModelManager().getActiveOntologies()) {
             for (OWLAxiom ax : ont.getAxioms(selectedClass)) {
                 if (ax.isLogicalAxiom() && !(ax instanceof OWLDisjointClassesAxiom)) {
@@ -52,8 +73,39 @@ public class DuplicateSelectedClassAction extends SelectedOWLClassAction {
                 }
             }
         }
+        return changes;
+    }
 
-        getOWLModelManager().applyChanges(changes);
-        getOWLWorkspace().getOWLSelectionModel().setSelectedEntity(set.getOWLEntity());
+    /**
+     * Duplicates all annotations on the class except those that are being used
+     * as a label to render the classes (to avoid name duplication).
+     * These are determined by matching the value to the name of the selected
+     * class and the URI to one of those being used by the annot renderer
+     * @param selectedClass the class to duplicate
+     * @param dup the OWL Object duplicator
+     * @return a list of changes
+     */
+    private List<OWLOntologyChange> duplicateAnnotations(OWLClass selectedClass, OWLObjectDuplicator dup) {
+        List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+        OWLModelManagerEntityRenderer ren = getOWLModelManager().getOWLEntityRenderer();
+        List<URI> annotURIs = null;
+        String selectedClassName = null;
+        if (ren instanceof OWLEntityAnnotationValueRenderer){
+            selectedClassName = ren.render(selectedClass);
+            annotURIs = OWLRendererPreferences.getInstance().getAnnotationURIs();
+        }
+
+        for (OWLOntology ont : getOWLModelManager().getActiveOntologies()) {
+            for (OWLAnnotationAxiom ax : selectedClass.getAnnotationAxioms(ont)){
+                final OWLAnnotation annot = ax.getAnnotation();
+                if (annotURIs == null ||
+                    !annotURIs.contains(annot.getAnnotationURI()) ||
+                    !annot.getAnnotationValueAsConstant().getLiteral().equals(selectedClassName)){
+                    OWLAxiom duplicatedAxiom = dup.duplicateObject(ax);
+                    changes.add(new AddAxiom(ont, duplicatedAxiom));
+                }
+            }
+        }
+        return changes;
     }
 }
