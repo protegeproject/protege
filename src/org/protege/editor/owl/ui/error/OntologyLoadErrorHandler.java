56cf68f678f9b515eb6556032d5fca904c5e146e
Nick Drummond <nickdrummond@yahoo.com>
2008-08-28 16:17:01 +0000

    - partial bugfix #585 Parser errors: more helpful - implemented a dialog that allows the user to filter on the format

---
+++ src/org/protege/editor/owl/ui/error/OntologyLoadErrorHandler.java
@@ -0,0 +1,48 @@
+package org.protege.editor.owl.ui.error;
+
+import org.semanticweb.owl.model.OWLOntologyCreationException;
+
+import java.net.URI;
+/*
+* Copyright (C) 2007, University of Manchester
+*
+* Modifications to the initial code base are copyright of their
+* respective authors, or their employers as appropriate.  Authorship
+* of the modifications may be determined from the ChangeLog placed at
+* the end of this file.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Author: drummond<br>
+ * http://www.cs.man.ac.uk/~drummond/<br><br>
+ * <p/>
+ * The University Of Manchester<br>
+ * Bio Health Informatics Group<br>
+ * Date: Aug 28, 2008<br><br>
+ *
+ * A hook to allow for feedback when an error occurs during loading
+ */
+public interface OntologyLoadErrorHandler {
+
+    /**
+     *
+     * @param uri the URI of the ontology that failed to load
+     * @param e the exception thrown by the OWL API
+     * @throws org.semanticweb.owl.model.OWLOntologyCreationException rethrow if the error needs to be reported further
+     */
+    void handleErrorLoadingOntology(URI uri, OWLOntologyCreationException e) throws OWLOntologyCreationException;
+}
