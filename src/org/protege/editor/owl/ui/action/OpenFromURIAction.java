f2fc4f78f3b5208b404aa8fb4b19c5244660f836
Nick Drummond <nickdrummond@yahoo.com>
2008-09-12 09:35:19 +0000

    - bugfix #700 Open from URI: does not prompt to open in new window - also recent URIs are added to the bookmarks list automatically when loaded

--- src/org/protege/editor/owl/ui/action/OpenFromURIAction.java
+++ src/org/protege/editor/owl/ui/action/OpenFromURIAction.java
@@ -1,10 +1,13 @@
 package org.protege.editor.owl.ui.action;
 
+import org.protege.editor.core.ProtegeManager;
+import org.protege.editor.core.editorkit.EditorKitFactoryPlugin;
+import org.protege.editor.core.ui.OpenFromURIPanel;
+import org.protege.editor.core.ui.error.ErrorLogPanel;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.net.URI;
-
-import org.protege.editor.core.ui.OpenFromURIPanel;
-import org.semanticweb.owl.model.OWLRuntimeException;
 
 
 /**
@@ -16,15 +19,39 @@ import org.semanticweb.owl.model.OWLRuntimeException;
 public class OpenFromURIAction extends ProtegeOWLAction {
 
     public void actionPerformed(ActionEvent e) {
-        URI uri = OpenFromURIPanel.showDialog();
-        if (uri != null) {
-            try {
-                getOWLEditorKit().handleLoadFrom(uri);
+        try {
+            int ret = JOptionPane.showConfirmDialog(getWorkspace(),
+                                                    "Do you want to open the ontology in a new frame?",
+                                                    "Open in new frame",
+                                                    JOptionPane.YES_NO_CANCEL_OPTION,
+                                                    JOptionPane.QUESTION_MESSAGE);
+
+            if (ret == JOptionPane.NO_OPTION) {
+                URI uri = getURI();
+                if (uri != null) {
+                    getOWLEditorKit().handleLoadFrom(uri);
+                }
             }
-            catch (Exception e1) {
-                throw new OWLRuntimeException(e1);
+            else if (ret == JOptionPane.YES_OPTION) {
+                URI uri = getURI();
+                if (uri != null) {
+                    for (EditorKitFactoryPlugin plugin : ProtegeManager.getInstance().getEditorKitFactoryPlugins()) {
+                        if (plugin.getId().equals(getEditorKit().getEditorKitFactory().getId())) {
+                            ProtegeManager.getInstance().loadAndSetupEditorKitFromURI(plugin, uri);
+                            break;
+                        }
+                    }
+                }
             }
         }
+        catch (Exception e1) {
+            ErrorLogPanel.showErrorDialog(e1);
+        }
+    }
+
+
+    private URI getURI() {
+        return OpenFromURIPanel.showDialog();
     }
 
 
