eb3cf73a7acda788695b84526ced563655305c15
Nick Drummond <nickdrummond@yahoo.com>
2008-09-19 15:40:51 +0000

    Javadoc for OWLEntityFactory

--- src/org/protege/editor/owl/model/entity/OWLEntityFactory.java
+++ src/org/protege/editor/owl/model/entity/OWLEntityFactory.java
@@ -16,26 +16,58 @@ import java.net.URI;
  */
 public interface OWLEntityFactory {
 
+    /**
+     *
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
+     * @return an object wrapping the changes that need to be applied
+     * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
+     */
     public OWLEntityCreationSet<OWLClass> createOWLClass(String shortName, URI baseURI) throws OWLEntityCreationException;
 
-
+    /**
+     *
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
+     * @return an object wrapping the changes that need to be applied
+     * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
+     */
     public OWLEntityCreationSet<OWLObjectProperty> createOWLObjectProperty(String shortName, URI baseURI) throws OWLEntityCreationException;
 
-
+    /**
+     *
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
+     * @return an object wrapping the changes that need to be applied
+     * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
+     */
     public OWLEntityCreationSet<OWLDataProperty> createOWLDataProperty(String shortName, URI baseURI) throws OWLEntityCreationException;
 
-
+    /**
+     *
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
+     * @return an object wrapping the changes that need to be applied
+     * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
+     */
     public OWLEntityCreationSet<OWLIndividual> createOWLIndividual(String shortName, URI baseURI) throws OWLEntityCreationException;
 
-
+    /**
+     *
+     * @param type OWLClass, OWLObjectProperty, OWLDataProperty or OWLIndividual
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
+     * @return an object wrapping the changes that need to be applied
+     * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
+     */
     public <T extends OWLEntity> OWLEntityCreationSet<T> createOWLEntity(Class<T> type, String shortName, URI baseURI) throws OWLEntityCreationException;
 
 
     /**
      * Use this to check if the entity can be created without affecting any generated IDs
-     * @param type
-     * @param shortName
-     * @param baseURI
+     * @param type OWLClass, OWLObjectProperty, OWLDataProperty or OWLIndividual
+     * @param shortName user supplied name
+     * @param baseURI specify a base or leave as null to let the factory decide
      * @throws OWLEntityCreationException if the entity could not be created because of bad input/name clashes/auto ID etc
      */
     public void tryCreate(Class<? extends OWLEntity> type, String shortName, URI baseURI) throws OWLEntityCreationException;
