a111eb75d39953b7ba38aa1652fa097191a80dc0
Nick Drummond <nickdrummond@yahoo.com>
2008-12-03 15:14:13 +0000

    - move axioms by type - tidied up interface

--- src/org/protege/editor/owl/ui/ontology/wizard/move/bytype/AxiomTypeSelectorPanel.java
+++ src/org/protege/editor/owl/ui/ontology/wizard/move/bytype/AxiomTypeSelectorPanel.java
@@ -3,14 +3,16 @@ package org.protege.editor.owl.ui.ontology.wizard.move.bytype;
 import org.protege.editor.core.ui.list.MList;
 import org.protege.editor.core.ui.list.MListItem;
 import org.protege.editor.core.ui.list.MListSectionHeader;
+import org.protege.editor.core.ui.list.RemovableObjectList;
 import org.protege.editor.owl.ui.ontology.wizard.move.MoveAxiomsKitConfigurationPanel;
 import org.semanticweb.owl.model.AxiomType;
-import org.semanticweb.owl.model.OWLAxiom;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 /*
 * Copyright (C) 2007, University of Manchester
@@ -47,7 +49,11 @@ public class AxiomTypeSelectorPanel extends MoveAxiomsKitConfigurationPanel {
 
     private MoveAxiomsByTypeKit kit;
 
-    private MList typeSelector2;
+    private MList typeSource;
+
+    private JButton button;
+
+    private RemovableObjectList<AxiomType> typeSelection;
 
 
     public AxiomTypeSelectorPanel(MoveAxiomsByTypeKit kit) {
@@ -56,8 +62,89 @@ public class AxiomTypeSelectorPanel extends MoveAxiomsKitConfigurationPanel {
 
 
     public void initialise() {
-        setLayout(new BorderLayout());
-        typeSelector2 = new MList();
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        typeSource = new MList();
+        java.util.List data = createTypesByTypeList();
+
+        typeSource.setListData(data.toArray());
+
+        typeSelection = new RemovableObjectList<AxiomType>();
+        typeSelection.setCellRenderer(new DefaultListCellRenderer(){
+            public Component getListCellRendererComponent(JList jList, Object o, int i, boolean b, boolean b1) {
+                if (o instanceof RemovableObjectList.RemovableObjectListItem){
+                    o = ((RemovableObjectList.RemovableObjectListItem)o).getObject();
+                }
+                return super.getListCellRendererComponent(jList, o, i, b, b1);
+            }
+        });
+
+        button = new JButton(new AbstractAction(">>") {
+            public void actionPerformed(ActionEvent e) {
+                addSelectedItems();
+            }
+        });
+
+        JComponent buttonPanel = new Box(BoxLayout.PAGE_AXIS);
+        buttonPanel.add(Box.createVerticalGlue());
+        buttonPanel.add(button);
+        buttonPanel.add(Box.createVerticalGlue());
+
+        add(Box.createHorizontalGlue());
+        add(new JScrollPane(typeSource));
+        add(buttonPanel);
+        add(new JScrollPane(typeSelection));
+        add(Box.createHorizontalGlue());
+    }
+
+
+    private void addSelectedItems() {
+        Set<AxiomType> selectedTypes = new HashSet<AxiomType>();
+        for (Object o : typeSource.getSelectedValues()){
+            if (o instanceof TypeItem){
+                final AxiomType axiomType = ((TypeItem) o).getType();
+                    selectedTypes.add(axiomType);
+            }
+        }
+        selectedTypes.removeAll(typeSelection.getListItems()); // don't allow duplicates
+        typeSelection.addObject(selectedTypes);
+    }
+
+
+    private Set<AxiomType> getSelection(){
+        return new HashSet<AxiomType>(typeSelection.getListItems());
+    }
+
+
+    public void dispose() {
+        // do nothing
+    }
+
+
+    public String getID() {
+        return getClass().getName();
+    }
+
+
+    public String getTitle() {
+        return "Axioms by type";
+    }
+
+
+    public String getInstructions() {
+        return "Please select the types of axiom you would like to move/copy.";
+    }
+
+
+    public void update() {
+    }
+
+
+    public void commit() {
+        kit.setTypes(getSelection());
+    }
+
+    private List createTypesByTypeList() {
         java.util.List data = new ArrayList();
         data.add(new MListSectionHeader(){
             public String getName() {
@@ -151,39 +238,7 @@ public class AxiomTypeSelectorPanel extends MoveAxiomsKitConfigurationPanel {
         data.add(new TypeItem(AxiomType.IMPORTS_DECLARATION));
         data.add(new TypeItem(AxiomType.DISJOINT_UNION));
 
-        typeSelector2.setListData(data.toArray());
-
-        add(new JScrollPane(typeSelector2), BorderLayout.CENTER);
-    }
-
-
-    public void dispose() {
-        // do nothing
-    }
-
-
-    public String getID() {
-        return getClass().getName();
-    }
-
-
-    public String getTitle() {
-        return "Select axiom types";
-    }
-
-
-    public void update() {
-    }
-
-
-    public void commit() {
-        Set<AxiomType<? extends OWLAxiom>> selectedTypes = new HashSet<AxiomType<? extends OWLAxiom>>();
-        for (Object o : typeSelector2.getSelectedValues()){
-            if (o instanceof TypeItem){
-                selectedTypes.add(((TypeItem)o).getType());
-            }
-        }
-        kit.setTypes(selectedTypes);
+        return data;
     }
 
 
