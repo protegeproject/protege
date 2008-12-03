e7e3b6cb65039658439e4931a0d90d68d57deb1d
Nick Drummond <nickdrummond@yahoo.com>
2008-12-03 16:51:35 +0000

    - move axioms by profile implementation - there are some bugs in the OWL API profiles, but MJH is going to be rewriting some of this soon

---
+++ src/org/protege/editor/owl/ui/ontology/wizard/move/byprofile/MoveAxiomsByProfileKit.java
@@ -0,0 +1,90 @@
+package org.protege.editor.owl.ui.ontology.wizard.move.byprofile;
+
+import org.protege.editor.owl.ui.ontology.wizard.move.MoveAxiomsKit;
+import org.protege.editor.owl.ui.ontology.wizard.move.MoveAxiomsKitConfigurationPanel;
+import org.semanticweb.owl.model.OWLAxiom;
+import org.semanticweb.owl.model.OWLOntology;
+import org.semanticweb.owl.profiles.ConstructNotAllowed;
+import org.semanticweb.owl.profiles.OWLProfile;
+import org.semanticweb.owl.profiles.OWLProfileReport;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+/*
+* Copyright (C) 2007, University of Manchester
+*
+* Modifications to the initial code base are copyright of their
+* respective authors, or their employers as appropriate.  Authorship
+* of the modifications may be determined from the ChangeLog placed at
+* the end of this file.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Author: drummond<br>
+ * http://www.cs.man.ac.uk/~drummond/<br><br>
+ * <p/>
+ * The University Of Manchester<br>
+ * Bio Health Informatics Group<br>
+ * Date: Dec 3, 2008<br><br>
+ */
+public class MoveAxiomsByProfileKit extends MoveAxiomsKit {
+
+    private ProfileSelectorPanel profileSelectorPanel;
+
+    private OWLProfile profile;
+
+
+    public List<MoveAxiomsKitConfigurationPanel> getConfigurationPanels() {
+        List<MoveAxiomsKitConfigurationPanel> panels = new ArrayList<MoveAxiomsKitConfigurationPanel>();
+        panels.add(profileSelectorPanel);
+        return panels;
+    }
+
+
+    public Set<OWLAxiom> getAxioms(Set<OWLOntology> sourceOntologies) {
+        Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+        for (OWLOntology ont: sourceOntologies){
+            axioms.addAll(ont.getAxioms());
+        }
+        for (OWLOntology ont : sourceOntologies){
+            OWLProfileReport report = profile.checkOntology(ont, getOWLModelManager().getOWLOntologyManager());
+            for (ConstructNotAllowed disConstr : report.getDisallowedConstructs()){
+                if (disConstr.getConstruct() instanceof OWLAxiom){
+                    axioms.remove(disConstr.getConstruct());
+                }
+            }
+        }
+        return axioms;
+    }
+
+
+    public void initialise() throws Exception {
+        profileSelectorPanel = new ProfileSelectorPanel(this);
+    }
+
+
+    public void dispose() throws Exception {
+        profileSelectorPanel.dispose();
+    }
+
+
+    public void setProfile(OWLProfile profile){
+        this.profile = profile;
+    }
+}
