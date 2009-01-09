bb9c2675c867ea418cf93504c625fdeb70a97382
Nick Drummond <nickdrummond@yahoo.com>
2009-01-09 13:51:07 +0000

    - #840 Class Hierarchy: selection when multiple nodes in tree - moving/deleting nodes in the tree was not updating the nodeMap in OWLObjectTree correctly

--- src/org/protege/editor/owl/ui/clshierarchy/AbstractOWLClassHierarchyViewComponent.java
+++ src/org/protege/editor/owl/ui/clshierarchy/AbstractOWLClassHierarchyViewComponent.java
@@ -18,9 +18,8 @@ import org.semanticweb.owl.model.OWLObject;
 import org.semanticweb.owl.util.OWLEntitySetProvider;
 import org.semanticweb.owl.vocab.OWLRDFVocabulary;
 
-import javax.swing.event.ChangeListener;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
+import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.tree.TreeCellRenderer;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -81,6 +80,25 @@ public abstract class AbstractOWLClassHierarchyViewComponent extends AbstractOWL
         if (cls != null) {
             setSelectedClass(cls);
         }
+        tree.getModel().addTreeModelListener(new TreeModelListener() {
+            public void treeNodesChanged(TreeModelEvent e) {
+            }
+
+
+            public void treeNodesInserted(TreeModelEvent e) {
+                ensureSelection();
+            }
+
+
+            public void treeNodesRemoved(TreeModelEvent e) {
+                ensureSelection();
+            }
+
+
+            public void treeStructureChanged(TreeModelEvent e) {
+                ensureSelection();
+            }
+        });
         tree.addMouseListener(new MouseAdapter() {
             public void mouseReleased(MouseEvent e) {
                 transmitSelection();
@@ -100,12 +118,10 @@ public abstract class AbstractOWLClassHierarchyViewComponent extends AbstractOWL
         tree.setSelectedOWLObject(cls);
     }
 
-
     public void setSelectedClasses(Set<OWLClass> clses) {
         tree.setSelectedOWLObjects(clses);
     }
 
-    
     public OWLClass getSelectedClass() {
         return tree.getSelectedOWLObject();
     }
@@ -115,9 +131,23 @@ public abstract class AbstractOWLClassHierarchyViewComponent extends AbstractOWL
         return new HashSet<OWLClass>(tree.getSelectedOWLObjects());
     }
 
-
     private boolean isNothing(OWLClass o1) {
         return o1.getURI().equals(OWLRDFVocabulary.OWL_NOTHING.getURI());
+    }
+
+
+    private void ensureSelection() {
+        final OWLClass cls = getSelectedOWLClass();
+        if (cls != null) {
+            OWLClass treeSel = tree.getSelectedOWLObject();
+            if (treeSel == null || !treeSel.equals(cls)) {
+                SwingUtilities.invokeLater(new Runnable(){
+                    public void run() {
+                        tree.setSelectedOWLObject(cls);
+                    }
+                });
+            }
+        }
     }
 
 
