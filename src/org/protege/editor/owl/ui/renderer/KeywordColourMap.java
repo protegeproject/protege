bb13e9b85f29c03d4a7c5c67381291e022d17d17
Nick Drummond <nickdrummond@yahoo.com>
2009-07-03 15:35:18 +0000

    Hyperlinking of annotation properties Keyword highlighting using OWL API keyword categories

--- src/org/protege/editor/owl/ui/renderer/KeywordColourMap.java
+++ src/org/protege/editor/owl/ui/renderer/KeywordColourMap.java
@@ -1,5 +1,7 @@
 package org.protege.editor.owl.ui.renderer;
 
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntax;
+
 import java.awt.*;
 import java.util.HashMap;
 /*
@@ -39,47 +41,28 @@ public class KeywordColourMap extends HashMap<String, Color> {
         Color restrictionColor = new Color(178, 0, 178);
         Color logicalOpColor = new Color(0, 178, 178);
         Color axiomColor = new Color(10, 94, 168);
-        Color queryColor = new Color(100, 15, 120);
-        put("some", restrictionColor);
-        put("only", restrictionColor);
-        put("value", restrictionColor);
-        put("exactly", restrictionColor);
-        put("min", restrictionColor);
-        put("max", restrictionColor);
-        put("inv", logicalOpColor);
-        put("and", logicalOpColor);
-        put("that", logicalOpColor);
-        put("or", logicalOpColor);
-        put("not", logicalOpColor);
-        put("subClassOf", axiomColor);
-        put("SubClassOf", axiomColor);
-        put("disjointWith", axiomColor);
-        put("DisjointWith", axiomColor);
-        put("equivalentTo", axiomColor);
-        put("EquivalentTo", axiomColor);
-        put("domain", axiomColor);
-        put("range", axiomColor);
-        put("instanceOf", axiomColor);
-        put("types", axiomColor);
-        put("InstanceOf", axiomColor);
-        put("minus", queryColor);
-        put("plus", queryColor);
-        put("possibly", queryColor);
-        put("inverseOf", axiomColor);
-        put("DifferentIndividuals:", axiomColor);
-        put("SameIndividuals:", axiomColor);
-        put("Functional:", axiomColor);
-        put("InverseFunctional:", axiomColor);
-        put("Symmetric:", axiomColor);
-        put("AntiSymmetric:", axiomColor);
-        put("Reflexive:", axiomColor);
-        put("Irreflexive:", axiomColor);
-        put("Transitive:", axiomColor);
-        put("subPropertyOf", axiomColor);
-        put("disjointUnionOf", axiomColor);
+        Color typeColor = new Color(178, 178, 178);
+
+        for (ManchesterOWLSyntax keyword : ManchesterOWLSyntax.values()){
+            if (keyword.isAxiomKeyword()){
+                put(keyword.toString(), axiomColor);
+                put(keyword.toString() + ":", axiomColor);
+            }
+            else if (keyword.isClassExpressionConnectiveKeyword()){
+                put(keyword.toString(), logicalOpColor);
+            }
+            else if (keyword.isClassExpressionQuantiferKeyword()){
+                put(keyword.toString(), restrictionColor);
+            }
+            else if (keyword.isSectionKeyword()){
+                put(keyword.toString(), typeColor);
+                put(keyword.toString() + ":", typeColor);
+            }
+        }
+
         put("o", axiomColor);
-        put("\u279E", axiomColor);
-        put("\u2192", axiomColor);
-        put("\u2227", axiomColor);
+//        put("\u279E", axiomColor);
+//        put("\u2192", axiomColor);
+//        put("\u2227", axiomColor);
     }
 }
