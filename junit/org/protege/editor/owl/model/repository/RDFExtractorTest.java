7d15d0b59f9fb5ab49fa7b15bb443eb32b61658e
Timothy Redmond <tredmond@stanford.edu>
2009-11-18 07:07:39 +0000

    junits run in the individual project

--- junit/org/protege/editor/owl/model/repository/RDFExtractorTest.java
+++ junit/org/protege/editor/owl/model/repository/RDFExtractorTest.java
@@ -3,5 +3,7 @@ package org.protege.editor.owl.model.repository;
 import junit.framework.TestCase;
 
 public class RDFExtractorTest extends TestCase {
-
+    public void testExtractor() {
+        
+    }
 }
