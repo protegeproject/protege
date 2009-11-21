35c2d55d5d90e1603f36050d4f2a0e968c5e52f1
Timothy Redmond <tredmond@stanford.edu>
2009-11-21 15:47:26 +0000

    refactoring build scripts

--- osgi/knopflerfish/run.sh
+++ osgi/knopflerfish/run.sh
@@ -2,6 +2,5 @@
 
 #DEBUG_OPT="-Xdebug -Xrunjdwp:transport=dt_socket,address=8500,server=y,suspend=y"
 java -Dlog4j.configuration=file:log4j.xml ${DEBUG_OPT} \
-     -Dorg.protege.plugin.dir=jars/plugins \
      -classpath framework.jar:crimson.jar \
      org.knopflerfish.framework.Main
