67f57c1ec5ba1e474754a421fc6e082190a633eb
Timothy Redmond <tredmond@stanford.edu>
2009-11-22 08:09:23 +0000

    moving the latest felix for osgi 4.2 features

--- osgi/felix/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf ${HOME}/.felix/Protege
-java -Dlog4j.configuration=file:log4j.xml \
-     -Dorg.protege.plugin.dir=plugins \
-     -classpath bin/felix.jar:bin/crimson.jar \
-     org.apache.felix.main.Main
