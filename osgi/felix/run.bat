ba4ece29d61a2ec1236b49cedeca62e20de7ea19
Timothy Redmond <tredmond@stanford.edu>
2009-12-13 18:12:51 +0000

    included some standardoptions in felix.

--- osgi/felix/run.bat
+++ osgi/felix/run.bat
@@ -1,2 +1,2 @@
 
-java -Dlog4j.configuration=file:log4j.xml -Dorg.protege.plugin.dir=plugins -classpath bin/felix.jar:bin/crimson.jar org.apache.felix.main.Main
+java -Dlog4j.configuration=file:log4j.xml -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -Dapple.laf.useScreenMenuBar=true -Dcom.apple.mrj.application.growbox.intrudes=true -Dorg.protege.plugin.dir=plugins -classpath bin/felix.jar:bin/crimson.jar org.apache.felix.main.Main
--- osgi/felix/run.sh
+++ osgi/felix/run.sh
@@ -2,5 +2,9 @@
 
 java -Dlog4j.configuration=file:log4j.xml \
      -Dorg.protege.plugin.dir=plugins \
+     -DentityExpansionLimit=100000000 \
+     -Dfile.encoding=utf-8 \
+     -Dapple.laf.useScreenMenuBar=true  \
+     -Dcom.apple.mrj.application.growbox.intrudes=true \
      -classpath bin/felix.jar:bin/crimson.jar \
      org.apache.felix.main.Main
