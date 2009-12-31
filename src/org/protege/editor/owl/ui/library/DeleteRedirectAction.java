ab51fc6e963a9f1c030948a89a030577169dc2d8
Timothy Redmond <tredmond@stanford.edu>
2009-12-31 00:53:56 +0000

    Added the edit uri xml catalog entry method

---
+++ src/org/protege/editor/owl/ui/library/DeleteRedirectAction.java
@@ -0,0 +1,51 @@
+package org.protege.editor.owl.ui.library;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+
+import org.protege.editor.owl.model.library.OntologyLibrary;
+import org.protege.xmlcatalog.entry.Entry;
+import org.protege.xmlcatalog.entry.GroupEntry;
+import org.protege.xmlcatalog.entry.NextCatalogEntry;
+
+public class DeleteRedirectAction extends AbstractAction {
+    private JTree tree;
+    private TreePath selectionPath;
+    
+    public DeleteRedirectAction(JTree tree, TreePath selectionPath) {
+        super("Delete Library Entry");
+        this.selectionPath = selectionPath;
+        this.tree = tree;
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        DefaultMutableTreeNode nodeToDelete = (DefaultMutableTreeNode) selectionPath.getLastPathComponent();
+        boolean deleteFromTree = false;
+        Entry toDelete = (Entry) nodeToDelete.getUserObject();
+        Object o = ((DefaultMutableTreeNode) selectionPath.getParentPath().getLastPathComponent()).getUserObject();
+        
+        if (o instanceof OntologyLibrary) {
+            OntologyLibrary lib = (OntologyLibrary) o;
+            lib.getXmlCatalog().removeEntry(toDelete);
+            deleteFromTree = true;
+        }
+        else if (o instanceof GroupEntry) {
+            GroupEntry group = (GroupEntry) o;
+            group.removeEntry(toDelete);
+            deleteFromTree = true;
+        }
+        else if (o instanceof NextCatalogEntry) {
+            NextCatalogEntry subCatalogEntry = (NextCatalogEntry) o;
+            throw new UnsupportedOperationException("Not implemented yet");
+        }
+        if (deleteFromTree)  {
+            ((DefaultTreeModel) tree.getModel()).removeNodeFromParent(nodeToDelete);
+            tree.repaint();
+        }
+    }
+}
