7324009da744b2a64e40c2bf34db4cee211f8b97
Timothy Redmond <tredmond@stanford.edu>
2010-02-09 06:27:33 +0000

    fixed up crimson things that didn't seem to get in.

--- osgi/felix/run.sh
+++ osgi/felix/run.sh
@@ -6,6 +6,5 @@ java -Dlog4j.configuration=file:log4j.xml \
      -Dfile.encoding=utf-8 \
      -Dapple.laf.useScreenMenuBar=true  \
      -Dcom.apple.mrj.application.growbox.intrudes=true \
-     -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl \
      -classpath bin/felix.jar \
      org.apache.felix.main.Main
