8eb05141b77a4789e13f9c0ffa45278efd0325ec
Timothy Redmond <tredmond@stanford.edu>
2010-02-09 06:27:33 +0000

    fixed up crimson things that didn't seem to get in.

--- osgi/felix/run.bat
+++ osgi/felix/run.bat
@@ -1,2 +1,2 @@
 
-java -Dlog4j.configuration=file:log4j.xml -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl  -Dorg.protege.plugin.dir=plugins -classpath bin/felix.jar;bin/crimson.jar org.apache.felix.main.Main 
+java -Dlog4j.configuration=file:log4j.xml -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bin/felix.jar org.apache.felix.main.Main 
