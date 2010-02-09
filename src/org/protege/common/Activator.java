ba602a4320f30de214ae36b9f9e6e082ab43c99b
Timothy Redmond <tredmond@stanford.edu>
2010-02-09 01:01:01 +0000

    felix and the Protege.app (based on felix) now run without crimson

---
+++ src/org/protege/common/Activator.java
@@ -0,0 +1,19 @@
+package org.protege.common;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+		context.registerService(
+	        javax.xml.parsers.SAXParserFactory.class.getName(),
+	        javax.xml.parsers.SAXParserFactory.newInstance(), null);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
