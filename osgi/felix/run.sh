6e075f5b251a747ae19edc3fdb346a573b33b391
Timothy Redmond <tredmond@stanford.edu>
2010-02-25 00:15:06 +0000

    restoring the pre-install-anywhere mode

--- osgi/felix/run.sh
+++ osgi/felix/run.sh
@@ -3,11 +3,10 @@
 cd `dirname $0`
 
 java -Dlog4j.configuration=file:log4j.xml \
-     -Dfelix.config.properties=file:conf/config.properties \
      -Dorg.protege.plugin.dir=plugins \
      -DentityExpansionLimit=100000000 \
      -Dfile.encoding=utf-8 \
      -Dapple.laf.useScreenMenuBar=true  \
      -Dcom.apple.mrj.application.growbox.intrudes=true \
-     -classpath felix.jar \
+     -classpath bin/felix.jar \
      org.apache.felix.main.Main
