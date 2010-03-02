967555e8dffb04fa6806471b5bdcc949c9ff310d
Timothy Redmond <tredmond@stanford.edu>
2010-03-02 04:23:24 +0000

    this seems like a useful class - after the build it should take over?

---
+++ src/org/protege/editor/core/plugin/AbstractProtegePlugin.java
@@ -0,0 +1,38 @@
+package org.protege.editor.core.plugin;
+
+import org.eclipse.core.runtime.IExtension;
+
+public abstract class AbstractProtegePlugin<P extends ProtegePluginInstance> implements ProtegePlugin<P> {
+	protected IExtension extension;
+	
+	protected AbstractProtegePlugin(IExtension extension) {
+		this.extension = extension;
+	}
+	
+	public IExtension getIExtension() {
+		return extension;
+	}
+
+    /**
+     * Gets a <code>String</code> that represents the reasoner ID.
+     */
+    public String getId() {
+        return extension.getUniqueIdentifier();
+    }
+    
+
+    public String getDocumentation() {
+        return JPFUtil.getDocumentation(extension);
+    }
+
+	public P newInstance() throws ClassNotFoundException,
+			IllegalAccessException, InstantiationException {
+        ExtensionInstantiator<P> instantiator = new ExtensionInstantiator<P>(extension);
+        return instantiator.instantiate();
+	}
+	
+	protected String getPluginProperty(String key, String defaultValue) {
+        return PluginProperties.getParameterValue(extension, key, defaultValue);
+	}
+
+}
