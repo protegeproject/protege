13f25700af080b8346d51906b0f0398c4871cbf1
Timothy Redmond <tredmond@stanford.edu>
2010-03-06 01:25:40 +0000

    The new reasoner preferences file is slowly getting filled in.

--- src/org/protege/editor/owl/model/inference/ReasonerPreferences.java
+++ src/org/protege/editor/owl/model/inference/ReasonerPreferences.java
@@ -1,9 +1,8 @@
 package org.protege.editor.owl.model.inference;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.EnumMap;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 import org.protege.editor.core.prefs.Preferences;
 import org.protege.editor.core.prefs.PreferencesManager;
@@ -13,18 +12,56 @@ public class ReasonerPreferences {
 
     public static final String SHOW_INFERENCES_KEY = "SHOW_INFERENCES";
     
+    private long startOperationTime;
+    
 	private boolean showInferences;
+	
+	private EnumMap<OptionalInferenceTask, Boolean>  enabledMap    = new EnumMap<OptionalInferenceTask, Boolean>(OptionalInferenceTask.class);
+	private EnumMap<OptionalInferenceTask, Integer> clockMap       = new EnumMap<OptionalInferenceTask, Integer>(OptionalInferenceTask.class);
+	private EnumMap<OptionalInferenceTask, Integer> countMap       = new EnumMap<OptionalInferenceTask, Integer>(OptionalInferenceTask.class);
+
+	
+	public enum OptionalInferenceTask {
+	    // Class Property Inferences
+	    SHOW_INFERRED_INHERITED_ANONYMOUS_CLASSES(false),
+	    // Object Property Inferences
+	    SHOW_OBJECT_PROPERTY_UNSATISFIABILITY(true),
+	    SHOW_INFERRED_OBJECT_PROPERTY_DOMAINS(false),
+	    SHOW_INFERRED_OBJECT_PROPERTY_RANGES(false),
+	    // Datatype Property Inferences
+	    SHOW_INFERRED_DATATYPE_PROPERTY_DOMAINS(false);
+	    
+	    private boolean enabledByDefault;
+	    
+	    private OptionalInferenceTask(boolean enabledByDefault) {
+	        this.enabledByDefault = enabledByDefault;
+	    }
+	    
+	    public String getKey() {
+            return toString();
+        }
+	    
+	    public boolean getEnabledByDefault() {
+	        return enabledByDefault;
+	    }
+	}
 	
 	public void load() {
         PreferencesManager prefMan = PreferencesManager.getInstance();
         Preferences prefs = prefMan.getPreferencesForSet(PREFERENCES_SET_KEY, ReasonerPreferences.class);
         showInferences = prefs.getBoolean(SHOW_INFERENCES_KEY, true);
+        for (OptionalInferenceTask task : OptionalInferenceTask.values()) {
+            enabledMap.put(task, prefs.getBoolean(task.getKey(), task.getEnabledByDefault()));
+        }
 	}
 	
 	public void save() {
         PreferencesManager prefMan = PreferencesManager.getInstance();
         Preferences prefs = prefMan.getPreferencesForSet(PREFERENCES_SET_KEY, ReasonerPreferences.class);
         prefs.putBoolean(SHOW_INFERENCES_KEY, showInferences);
+        for (OptionalInferenceTask task : OptionalInferenceTask.values()) {
+            prefs.putBoolean(task.getKey(), enabledMap.get(task));
+        }
 	}
 
 	public boolean isShowInferences() {
@@ -34,5 +71,57 @@ public class ReasonerPreferences {
 	public void setShowInferences(boolean showInferences) {
 		this.showInferences = showInferences;
 	}
+	
+	public void startClock(OptionalInferenceTask task) {
+	    startOperationTime = System.currentTimeMillis();
+	}
+	
+	public void stopClock(OptionalInferenceTask task) {
+	    int duration = (int) (System.currentTimeMillis() - startOperationTime);
+	    
+	    countMap.put(task, getCallCount(task) + 1);
+	    clockMap.put(task, getTimeInTask(task) + duration);
+	}
+	
+	public int getTimeInTask(OptionalInferenceTask task) {
+	    Integer duration = clockMap.get(task);
+	    if (duration == null) {
+	        duration = 0;
+	    }
+	    return duration;
+	}
+
+	public int getAverageTimeInTask(OptionalInferenceTask task) {
+	    int count = getCallCount(task);
+	    if (count == 0) {
+	        return 0;
+	    }
+	    return getTimeInTask(task) / count;
+	}
+	
+	private int getCallCount(OptionalInferenceTask task) {
+	    Integer count = countMap.get(task);
+	    if (count == null) {
+	        count = 0;
+	    }
+	    return count;
+	}
+	
+	public boolean isEnabled(OptionalInferenceTask task) {
+	    return enabledMap.get(task);
+	}
+	
+	public void setEnabled(OptionalInferenceTask task, boolean enabled) {
+	    enabledMap.put(task, enabled);
+	}
+	
+	public void executeTask(OptionalInferenceTask task, Runnable implementation)  {
+	    if (isShowInferences() && isEnabled(task)) {
+	        startClock(task);
+	        implementation.run();
+	        stopClock(task);
+	    }
+	}
+
 
 }
