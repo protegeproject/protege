5201ba08e70ac771a5ea54912504900ddad44757
Timothy Redmond <tredmond@stanford.edu>
2010-03-07 05:52:20 +0000

    finished the object properties tab

--- src/org/protege/editor/owl/ui/inference/ReasonerPreferencesPanel.java
+++ src/org/protege/editor/owl/ui/inference/ReasonerPreferencesPanel.java
@@ -21,9 +21,10 @@ public class ReasonerPreferencesPanel extends OWLPreferencesPanel {
 
     public void initialise() throws Exception {
         preferences = getOWLModelManager().getReasonerPreferences();
-        add(buildClassFrameListPreferences());
-        add(buildObjectPropertyFrameListPreferences());
-        add(buildDataPropertyFrameListPreferences());
+        add(buildClassFrameSectionPreferences());
+        add(buildObjectPropertyFrameSectionPreferences());
+        add(buildDataPropertyFrameSectionPreferences());
+        add(buildIndividualFrameSectionPreferences());
     }
 
     @Override
@@ -40,29 +41,40 @@ public class ReasonerPreferencesPanel extends OWLPreferencesPanel {
         enabledMap = null;
     }
     
-    private JComponent buildClassFrameListPreferences() {
-        Box classFrameListPreferences = new Box(BoxLayout.Y_AXIS);
-        classFrameListPreferences.setBorder(ComponentFactory.createTitledBorder("Class Inferences"));
-        classFrameListPreferences.setAlignmentX(0.0f);
-        classFrameListPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_INHERITED_ANONYMOUS_CLASSES, "Inferred Inherited Anonymous Classes"));
-        return classFrameListPreferences;
+    private JComponent buildClassFrameSectionPreferences() {
+        Box classFrameSectionPreferences = new Box(BoxLayout.Y_AXIS);
+        classFrameSectionPreferences.setBorder(ComponentFactory.createTitledBorder("Class Inferences"));
+        classFrameSectionPreferences.setAlignmentX(0.0f);
+        classFrameSectionPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_CLASS_UNSATISFIABILITY, "Unsatisfiability"));
+        classFrameSectionPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_INHERITED_ANONYMOUS_CLASSES, "Inferred Inherited Classes"));
+        return classFrameSectionPreferences;
     }
     
-    private JComponent buildObjectPropertyFrameListPreferences() {
+    private JComponent buildObjectPropertyFrameSectionPreferences() {
         Box objectPropertyTabPreferences = new Box(BoxLayout.Y_AXIS);
         objectPropertyTabPreferences.setBorder(ComponentFactory.createTitledBorder("Object Property Inferences"));
         objectPropertyTabPreferences.setAlignmentX(0.0f);
         objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_OBJECT_PROPERTY_UNSATISFIABILITY, "Unsatisfiability"));
         objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_OBJECT_PROPERTY_DOMAINS, "Inferred Domains"));
         objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_OBJECT_PROPERTY_RANGES, "Inferred Ranges"));
+        objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_EQUIVALENT_OBJECT_PROPERTIES, "Inferred Equivalents"));
+        objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_SUPER_OBJECT_PROPERTIES, "Inferred SuperProperties"));
+        objectPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_INVERSE_PROPERTIES, "Inferred Inverses"));
         return objectPropertyTabPreferences;
     }
     
-    private JComponent buildDataPropertyFrameListPreferences() {
+    private JComponent buildDataPropertyFrameSectionPreferences() {
         Box dataPropertyTabPreferences = new Box(BoxLayout.Y_AXIS);
         dataPropertyTabPreferences.setBorder(ComponentFactory.createTitledBorder("Data Property Inferences"));
         dataPropertyTabPreferences.setAlignmentX(0.0f);
         dataPropertyTabPreferences.add(getCheckBox(OptionalInferenceTask.SHOW_INFERRED_DATATYPE_PROPERTY_DOMAINS, "Inferred Domains"));
+        return dataPropertyTabPreferences;
+    }
+    
+    private JComponent buildIndividualFrameSectionPreferences() {
+        Box dataPropertyTabPreferences = new Box(BoxLayout.Y_AXIS);
+        dataPropertyTabPreferences.setBorder(ComponentFactory.createTitledBorder("Individual Inferences"));
+        dataPropertyTabPreferences.setAlignmentX(0.0f);
         return dataPropertyTabPreferences;
     }
     
