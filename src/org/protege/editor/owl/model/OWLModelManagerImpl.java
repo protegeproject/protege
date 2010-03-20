d7bfe52041dcc7e0b02435e26fa94b0ad2a9b3da
Timothy Redmond <tredmond@stanford.edu>
2010-03-20 03:07:08 +0000

    added the write safety back - test it

--- src/org/protege/editor/owl/model/OWLModelManagerImpl.java
+++ src/org/protege/editor/owl/model/OWLModelManagerImpl.java
@@ -184,6 +184,7 @@ public class OWLModelManagerImpl extends AbstractModelManager
         modelManagerListenerManager = new ListenerManager<OWLModelManagerListener>();
         changeListenerManager = new ListenerManager<OWLOntologyChangeListener>();
         manager = ProtegeOWLManager.createOWLOntologyManager();
+        manager.setUseWriteSafety(true);
         manager.setSilentMissingImportsHandling(true);
         manager.addOntologyChangeListener(this);
         manager.addOntologyLoaderListener(this);
