2c4259dec441d982f7a13babc84e6a7c03dfff8f
Timothy Redmond <tredmond@stanford.edu>
2010-04-30 00:04:21 +0000

    I think that this is dead code but I looked into refactoring it into the new plugin loader style also

--- src/org/protege/editor/core/ui/view/ViewActionPluginJPFImpl.java
+++ src/org/protege/editor/core/ui/view/ViewActionPluginJPFImpl.java
@@ -1,36 +1,12 @@
 package org.protege.editor.core.ui.view;
 
 
+import javax.swing.AbstractAction;
+
 import org.eclipse.core.runtime.IExtension;
 import org.protege.editor.core.editorkit.EditorKit;
-import org.protege.editor.core.plugin.ExtensionInstantiator;
-import org.protege.editor.core.plugin.PluginProperties;
 import org.protege.editor.core.ui.action.ProtegeAction;
 import org.protege.editor.core.ui.action.ToolBarActionPluginJPFImpl;
-
-import javax.swing.*;
-/*
- * Copyright (C) 2007, University of Manchester
- *
- * Modifications to the initial code base are copyright of their
- * respective authors, or their employers as appropriate.  Authorship
- * of the modifications may be determined from the ChangeLog placed at
- * the end of this file.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
-
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
 
 
 /**
@@ -59,13 +35,12 @@ public class ViewActionPluginJPFImpl extends ToolBarActionPluginJPFImpl implemen
 
 
     public String getViewId() {
-        return PluginProperties.getParameterValue(getExtension(), VIEW_ID_PARAM, null);
+        return getPluginProperty(VIEW_ID_PARAM);
     }
 
 
     public ProtegeAction newInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-        ExtensionInstantiator<ViewAction> instantiator = new ExtensionInstantiator<ViewAction>(getExtension());
-        ViewAction viewAction = instantiator.instantiate();
+        ViewAction viewAction = (ViewAction) super.newInstance();
         viewAction.putValue(AbstractAction.NAME, getName());
         viewAction.putValue(AbstractAction.SHORT_DESCRIPTION, getToolTipText());
         viewAction.setEditorKit(getEditorKit());
