40a9ebea43ce3eca714797c9c1f599a9f196c06c
Timothy Redmond <tredmond@stanford.edu>
2010-07-15 22:42:48 +0000

    a plugin loader for preferences about inferences.

---
+++ src/org/protege/editor/owl/ui/inference/InferencePreferencePluginLoader.java
@@ -0,0 +1,28 @@
+package org.protege.editor.owl.ui.inference;
+
+import org.eclipse.core.runtime.IExtension;
+import org.protege.editor.core.plugin.AbstractPluginLoader;
+import org.protege.editor.core.plugin.DefaultPluginExtensionMatcher;
+import org.protege.editor.core.plugin.PluginExtensionMatcher;
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ProtegeOWL;
+
+public class InferencePreferencePluginLoader extends AbstractPluginLoader<InferencePreferencePlugin> {
+    private OWLEditorKit editorKit;
+
+    public InferencePreferencePluginLoader(OWLEditorKit editorKit) {
+        super(ProtegeOWL.ID, InferencePreferencePlugin.ID);
+        this.editorKit = editorKit;
+    }
+
+    @Override
+    protected PluginExtensionMatcher getExtensionMatcher() {
+        return new DefaultPluginExtensionMatcher();
+    }
+
+    @Override
+    protected InferencePreferencePlugin createInstance(IExtension extension) {
+        return new InferencePreferencePlugin(editorKit, extension);
+    }
+
+}
