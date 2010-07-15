1ab1efbed9ad0a4151f1d578ee07e36d92377332
Timothy Redmond <tredmond@stanford.edu>
2010-07-15 22:36:05 +0000

    moved to the new plugin format

--- src/org/protege/editor/core/ui/preferences/PreferencesPanelPluginJPFImpl.java
+++ src/org/protege/editor/core/ui/preferences/PreferencesPanelPluginJPFImpl.java
@@ -3,9 +3,7 @@ package org.protege.editor.core.ui.preferences;
 
 import org.eclipse.core.runtime.IExtension;
 import org.protege.editor.core.editorkit.EditorKit;
-import org.protege.editor.core.plugin.ExtensionInstantiator;
-import org.protege.editor.core.plugin.JPFUtil;
-import org.protege.editor.core.plugin.PluginUtilities;
+import org.protege.editor.core.plugin.AbstractProtegePlugin;
 
 
 /**
@@ -17,42 +15,28 @@ import org.protege.editor.core.plugin.PluginUtilities;
  * matthew.horridge@cs.man.ac.uk<br>
  * www.cs.man.ac.uk/~horridgm<br><br>
  */
-public class PreferencesPanelPluginJPFImpl implements PreferencesPanelPlugin {
+public class PreferencesPanelPluginJPFImpl extends AbstractProtegePlugin<PreferencesPanel> implements PreferencesPanelPlugin {
 
     public static final String ID = "preferencespanel";
 
     public static final String LABEL_PARAM = "label";
 
-    private IExtension extension;
-
     private EditorKit editorKit;
 
 
     public PreferencesPanelPluginJPFImpl(IExtension extension, EditorKit editorKit) {
-        this.extension = extension;
+        super(extension);
         this.editorKit = editorKit;
     }
 
 
     public String getLabel() {
-        return PluginUtilities.getAttribute(extension, LABEL_PARAM);
+        return getPluginProperty(LABEL_PARAM);
     }
-
-
-    public String getId() {
-        return extension.getUniqueIdentifier();
-    }
-
-
-    public String getDocumentation() {
-        return JPFUtil.getDocumentation(extension);
-    }
-
 
     public PreferencesPanel newInstance() throws ClassNotFoundException, IllegalAccessException,
                                                  InstantiationException {
-        ExtensionInstantiator<PreferencesPanel> instantiator = new ExtensionInstantiator<PreferencesPanel>(extension);
-        PreferencesPanel preferencesPanel = instantiator.instantiate();
+        PreferencesPanel preferencesPanel = super.newInstance();
         preferencesPanel.setup(getLabel(), editorKit);
         return preferencesPanel;
     }
