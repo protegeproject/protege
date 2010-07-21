1688946069edc4bdd5ee443b66d75a3ac73e562c
Tania Tudorache <tudorache@stanford.edu>
2010-07-21 07:56:33 +0000

    If the auto generator setting for start id changes in a protege session, that change is immediately applied. GForge: https://bmir-gforge.stanford.edu/gf/project/owleditor/tracker/?action=TrackerItemEdit&tracker_item_id=2029&start=0

--- src/org/protege/editor/owl/model/entity/IterativeAutoIDGenerator.java
+++ src/org/protege/editor/owl/model/entity/IterativeAutoIDGenerator.java
@@ -37,20 +37,31 @@ import java.util.Stack;
  */
 public class IterativeAutoIDGenerator extends AbstractIDGenerator implements Revertable {
 
+	//TT: Not happy about this solution. The autoId generator is currently static,
+	//and we need a way to say if the startId has changed in the preferences,
+	//so that the generator will use the new start id.
+	private long previousStartId;
     private long id;
-
-    private long end;
 
     private Stack<Long> checkpoints = new Stack<Long>();
 
 
     public IterativeAutoIDGenerator() {
         id = EntityCreationPreferences.getAutoIDStart();
-        end = EntityCreationPreferences.getAutoIDEnd();
+        previousStartId = id;      
     }
 
 
     protected long getRawID(Class<? extends OWLEntity> type) throws AutoIDException{
+    	//check if start id prefs have changed meanwhile
+    	if (previousStartId != EntityCreationPreferences.getAutoIDStart()) {
+    		id = EntityCreationPreferences.getAutoIDStart();
+    		previousStartId = id;
+    		checkpoints.removeAllElements();
+    		checkpoints.push(id);
+    	}
+    	
+    	long end = EntityCreationPreferences.getAutoIDEnd();
         if (end != -1 && id > end){
             throw new AutoIDException("You have run out of IDs for creating new entities - max = " + end);
         }
