4a6dc3e10d541f811bc4586acd9422e719047634
Timothy Redmond <tredmond@stanford.edu>
2010-08-06 04:56:47 +0000

    more adjustments

--- osgi/equinox/run.sh
+++ osgi/equinox/run.sh
@@ -2,4 +2,9 @@
 
 cd `dirname $0`
 
-java ${CMD_OPTIONS} -Xms500M -Xmx500M -Dosgi.clean=true -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -jar org.eclipse.osgi.jar
+java ${CMD_OPTIONS} \
+     -Xms500M -Xmx500M \
+     -DentityExpansionLimit=100000000 \
+     -Dlog4j.configuration=file:log4j.xml \
+     -classpath org.eclipse.osgi.jar:ProtegeLauncher.jar \
+     org.protege.osgi.framework.Launcher
