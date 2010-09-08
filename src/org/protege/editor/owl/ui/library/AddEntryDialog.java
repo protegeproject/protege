e61c1eb88be481be2119d91c96f58edfceece251
Timothy Redmond <tredmond@stanford.edu>
2010-09-08 23:41:27 +0000

    a tabbed pane is a more normal option

--- src/org/protege/editor/owl/ui/library/AddEntryDialog.java
+++ src/org/protege/editor/owl/ui/library/AddEntryDialog.java
@@ -1,7 +1,6 @@
 package org.protege.editor.owl.ui.library;
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,8 +18,11 @@ import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.protege.editor.core.ProtegeApplication;
 import org.protege.editor.owl.model.library.CatalogEntryManager;
@@ -34,10 +36,7 @@ public class AddEntryDialog extends JDialog {
     
     private List<CatalogEntryManager> entryManagers;
     private XmlBaseContext xmlBase;
-    private JPanel cardedPanel;
-    private CardLayout cards;
-    private Map<String, NewEntryPanel> panels;
-    private String currentId;
+    private JTabbedPane tabs;
     private JButton ok;
     private boolean cancelled = false;
 
@@ -69,75 +68,34 @@ public class AddEntryDialog extends JDialog {
         this.entryManagers = entryManagers;
         this.xmlBase = xmlBase;
         setLayout(new BorderLayout());
-        add(createCenterPanel(), BorderLayout.CENTER);
+        add(createTabbedPane(), BorderLayout.CENTER);
         add(createButtons(), BorderLayout.SOUTH);
         pack();
         repaint();
     }
     
-    private JComponent createCenterPanel() {
-        JPanel centre = new JPanel();
-        centre.setLayout(new FlowLayout());
-        centre.add(createRadioButtons());
-        centre.add(new JSeparator(SwingConstants.VERTICAL));
-        centre.add(createCardedPanel());
-        return centre;
-    }
-    
-    private JComponent createRadioButtons() {
-        JPanel radioButtons = new JPanel();
-        radioButtons.setLayout(new BoxLayout(radioButtons, BoxLayout.Y_AXIS));
-        ButtonGroup group = new ButtonGroup();
-        for (CatalogEntryManager entryManager : entryManagers) {
-            final JCheckBox b = new JCheckBox(entryManager.getDescription());
-            group.add(b);
-            radioButtons.add(b);
-            final String id = entryManager.getId();
-            b.addActionListener(new ActionListener() {
-                
-                public void actionPerformed(ActionEvent e) {
-                    if (b.isSelected()) {
-                        currentId = id;
-                        cards.show(cardedPanel, id);
-                        ok.setSelected(panels.get(currentId).getEntry() != null);
-                    }
-                }
-            });
-            if (entryManager instanceof FolderGroupManager) {
-                b.setSelected(true);
-            }
-        }
-        radioButtons.setBorder(BorderFactory.createEtchedBorder());
-        return radioButtons;
-    }
-    
-    private JComponent createCardedPanel() {
-        cardedPanel = new JPanel();
-        cards = new CardLayout();
-        cardedPanel.setLayout(cards);
-        panels = new HashMap<String, NewEntryPanel>();
+    private JTabbedPane createTabbedPane() {
+    	tabs = new JTabbedPane();
         for (final CatalogEntryManager entryManager : entryManagers) {
-            String id = entryManager.getId();
-            final NewEntryPanel panel = entryManager.newEntryPanel(xmlBase);
-            panel.setAlignmentY(CENTER_ALIGNMENT);
-            panel.addListener(new Runnable() {
-                public void run() {
-                    if (entryManager.getId().equals(currentId)) {
-                        ok.setEnabled(panel.getEntry() != null);
-                    }
-                }
-            });
-            panels.put(id, panel);
-            cardedPanel.add(panel, id);
-            if (entryManager instanceof FolderGroupManager) {
-                currentId = id;
-            }
+        	NewEntryPanel panel = entryManager.newEntryPanel(xmlBase);
+        	panel.setAlignmentY(CENTER_ALIGNMENT);
+        	tabs.addTab(entryManager.getDescription(), panel);
+        	panel.addListener(new Runnable() {
+        		
+        		public void run() {
+        			updateOkButtonState();
+        		}
+        	});
         }
-        cards.layoutContainer(cardedPanel);
-        cards.show(cardedPanel, currentId);
-        return cardedPanel;
+        tabs.addChangeListener(new ChangeListener() {
+        	@Override
+        	public void stateChanged(ChangeEvent e) {
+        		updateOkButtonState();
+        	}
+        });
+        return tabs;
     }
-    
+
     private JComponent createButtons() {
         JPanel bottom = new JPanel();
         ok = new JButton("OK");
@@ -148,7 +106,7 @@ public class AddEntryDialog extends JDialog {
             }
         });
         ok.setAlignmentX(CENTER_ALIGNMENT);
-        ok.setEnabled(panels.get(currentId).getEntry() != null);
+        updateOkButtonState();
         bottom.add(ok);
         JButton cancel = new JButton("Cancel");
         cancel.addActionListener(new ActionListener() {
@@ -164,7 +122,15 @@ public class AddEntryDialog extends JDialog {
     }
     
     public Entry getEntry() {
-        return !cancelled ? panels.get(currentId).getEntry() : null;
+		NewEntryPanel panel = (NewEntryPanel) tabs.getSelectedComponent();
+        return !cancelled ? panel.getEntry() : null;
     }
 
+    
+    private void updateOkButtonState() {
+		NewEntryPanel panel = (NewEntryPanel) tabs.getSelectedComponent();
+		ok.setEnabled(panel.getEntry() != null);
+    }
+    
+    
 }
