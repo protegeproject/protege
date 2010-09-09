0ee8d27b022508399759b9d6036103a29db3665d
Timothy Redmond <tredmond@stanford.edu>
2010-09-09 22:04:21 +0000

    we are restricting to the case where a new ontology repository entry is placed at the top of the xml catalog.  Thus making it clear that the XMLBaseContext is an XMLCatalog makes all the code clearer.

--- src/org/protege/editor/owl/model/library/UriEntryManager.java
+++ src/org/protege/editor/owl/model/library/UriEntryManager.java
@@ -33,8 +33,8 @@ public class UriEntryManager extends CatalogEntryManager {
     }
 
     
-    public NewEntryPanel newEntryPanel(XmlBaseContext xmlBase) {
-        return new UriEntryPanel(xmlBase);
+    public NewEntryPanel newEntryPanel(XMLCatalog catalog) {
+        return new UriEntryPanel(catalog);
     }
 
     
