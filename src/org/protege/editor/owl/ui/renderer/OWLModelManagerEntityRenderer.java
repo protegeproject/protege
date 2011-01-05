f0037797f3d2a1d550b2b8ea4ecae2b996072ac9
Timothy Redmond <tredmond@stanford.edu>
2011-01-05 01:17:21 +0000

    Added new isConfigurable() and configure(OWLEditorKit) interfaces and removed a redundant interface.

--- src/org/protege/editor/owl/ui/renderer/OWLModelManagerEntityRenderer.java
+++ src/org/protege/editor/owl/ui/renderer/OWLModelManagerEntityRenderer.java
@@ -1,9 +1,12 @@
 package org.protege.editor.owl.ui.renderer;
 
 import org.protege.editor.core.Disposable;
+import org.protege.editor.core.plugin.ProtegePluginInstance;
+import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.util.ShortFormProvider;
 
 
 /**
@@ -15,22 +18,23 @@ import org.semanticweb.owlapi.model.OWLEntity;
  * matthew.horridge@cs.man.ac.uk<br>
  * www.cs.man.ac.uk/~horridgm<br><br>
  */
-public interface OWLModelManagerEntityRenderer extends OWLEntityRenderer, Disposable {
+public interface OWLModelManagerEntityRenderer extends ShortFormProvider, ProtegePluginInstance, Disposable {
 
     void setup(OWLModelManager owlModelManager);
 
-
     void initialise();
-
 
     String render(OWLEntity entity);
     
-    
     String render(IRI iri);
-
+    
+    void ontologiesChanged();
+    
+    boolean isConfigurable();
+    
+    boolean configure(OWLEditorKit eKit);
 
     void addListener(OWLEntityRendererListener listener);
-
 
     void removeListener(OWLEntityRendererListener listener);
 }
