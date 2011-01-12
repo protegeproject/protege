544a6dca1012708a5e2fc61a4c14a058af00f8f6
Timothy Redmond <tredmond@stanford.edu>
2011-01-12 01:09:15 +0000

    PrefixUtilities now contains a method that generates a prefix manager that is the union of all the prefixes in the active ontologies.

--- src/org/protege/editor/owl/ui/prefix/PrefixUtilities.java
+++ src/org/protege/editor/owl/ui/prefix/PrefixUtilities.java
@@ -1,12 +1,17 @@
 package org.protege.editor.owl.ui.prefix;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.protege.editor.owl.model.OWLModelManager;
+import org.protege.editor.owl.ui.renderer.prefix.ActiveOntologyComparator;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.PrefixManager;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.vocab.PrefixOWLOntologyFormat;
 
@@ -14,8 +19,23 @@ public class PrefixUtilities {
 
 	public static final Set<String> STANDARD_PREFIXES = Collections.unmodifiableSet(new DefaultPrefixManager().getPrefixNames());
 
-	public static PrefixOWLOntologyFormat getPrefixOWLOntologyFormat(OWLModelManager modelManager) {
-		return PrefixUtilities.getPrefixOWLOntologyFormat(modelManager.getActiveOntology());
+	public static PrefixManager getPrefixOWLOntologyFormat(OWLModelManager modelManager) {
+		OWLOntologyManager owlManager = modelManager.getOWLOntologyManager();
+		DefaultPrefixManager prefixes = new DefaultPrefixManager();
+		List<OWLOntology> ontologies = new ArrayList<OWLOntology>(modelManager.getActiveOntologies());
+		Collections.sort(ontologies, new ActiveOntologyComparator());
+		for (OWLOntology ontology : ontologies) {
+			OWLOntologyFormat format = owlManager.getOntologyFormat(ontology);
+			if (format instanceof PrefixOWLOntologyFormat) {
+				PrefixOWLOntologyFormat newPrefixes = (PrefixOWLOntologyFormat) format;
+				for (Entry<String, String> entry : newPrefixes.getPrefixName2PrefixMap().entrySet()) {
+					String prefixName = entry.getKey();
+					String prefix     = entry.getValue();
+					prefixes.setPrefix(prefixName, prefix);
+				}
+			}
+		}
+		return prefixes;
 	}
 
 	public static PrefixOWLOntologyFormat getPrefixOWLOntologyFormat(OWLOntology ontology) {
