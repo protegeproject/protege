7e8ec2f311577d44def64c9ff1ad4611f49331de
Timothy Redmond <tredmond@stanford.edu>
2011-01-21 00:13:49 +0000

    Menus to quickly (two-clicks) change from one render to the next.

---
+++ src/org/protege/editor/owl/ui/renderer/menu/RenderById.java
@@ -0,0 +1,22 @@
+package org.protege.editor.owl.ui.renderer.menu;
+
+import org.protege.editor.owl.ui.renderer.OWLEntityRendererImpl;
+import org.protege.editor.owl.ui.renderer.plugin.RendererPlugin;
+
+public class RenderById extends AbstractByRendererMenu {
+	private static final long serialVersionUID = 7364664042805363135L;
+
+	protected boolean isMyRendererPlugin(RendererPlugin plugin) {
+		return plugin.getRendererClassName().equals(OWLEntityRendererImpl.class.getName());
+	}
+	
+	@Override
+	protected boolean isConfigured(RendererPlugin plugin) {
+		return true;
+	}
+	
+	@Override
+	protected void configure(RendererPlugin plugin) {
+		;
+	}
+}
