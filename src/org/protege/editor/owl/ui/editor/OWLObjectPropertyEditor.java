6477f1f9a5f521dd0fbb907bb67b4654bcc0758f
Timothy Redmond <tredmond@stanford.edu>
2011-01-23 11:54:29 +0000

    Even though this editor can't have invalid contents, it needs to be a VerifiedInputEditor so that the caller can check the contents of the edited objects and verify that they satisfy an outside constraint.
    GForge 2144.   Certain FrameList editors allowed axioms that showed no objects and were not allowed by the specification.  An example was the disjoint classes axiom that would create an all disjoint classes axiom saying that a class was disjoint from nothing else.
    
    To test this we need to check 16 cases:
    
    (Equivalent and Disjoint) x (Classes, Object Properties, DataProperties)
    Same and Different Individuals
    
                   x FrameSection/FrameSectionRow.

--- src/org/protege/editor/owl/ui/editor/OWLObjectPropertyEditor.java
+++ src/org/protege/editor/owl/ui/editor/OWLObjectPropertyEditor.java
@@ -1,10 +1,15 @@
 package org.protege.editor.owl.ui.editor;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.JComponent;
+
+import org.protege.editor.core.ui.util.InputVerificationStatusChangedListener;
+import org.protege.editor.core.ui.util.VerifiedInputEditor;
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.selector.OWLObjectPropertySelectorPanel;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
-
-import javax.swing.*;
 
 
 /**
@@ -13,13 +18,21 @@ import javax.swing.*;
  * Bio-Health Informatics Group<br>
  * Date: 16-Feb-2007<br><br>
  */
-public class OWLObjectPropertyEditor extends AbstractOWLObjectEditor<OWLObjectProperty> {
+public class OWLObjectPropertyEditor extends AbstractOWLObjectEditor<OWLObjectProperty> implements VerifiedInputEditor {
 
     private OWLObjectPropertySelectorPanel editor;
 
+    private Set<InputVerificationStatusChangedListener> listeners = new HashSet<InputVerificationStatusChangedListener>();
+    
+    private InputVerificationStatusChangedListener inputListener = new InputVerificationStatusChangedListener(){
+        public void verifiedStatusChanged(boolean newState) {
+            handleVerifyEditorContents();
+        }
+    };
 
     public OWLObjectPropertyEditor(OWLEditorKit owlEditorKit) {
         editor = new OWLObjectPropertySelectorPanel(owlEditorKit);
+        editor.addStatusChangedListener(inputListener);
     }
 
 
@@ -46,9 +59,27 @@ public class OWLObjectPropertyEditor extends AbstractOWLObjectEditor<OWLObjectPr
     public JComponent getEditorComponent() {
         return editor;
     }
+    
+    private void handleVerifyEditorContents() {
+    	for (InputVerificationStatusChangedListener l : listeners){
+    		l.verifiedStatusChanged(true);
+    	}
+    }
+    
+    public void addStatusChangedListener(InputVerificationStatusChangedListener l) {
+        listeners.add(l);
+        l.verifiedStatusChanged(true);
+    }
 
 
+    public void removeStatusChangedListener(InputVerificationStatusChangedListener l) {
+        listeners.remove(l);
+    }
+	
     public void dispose() {
         editor.dispose();
     }
+
+
+
 }
