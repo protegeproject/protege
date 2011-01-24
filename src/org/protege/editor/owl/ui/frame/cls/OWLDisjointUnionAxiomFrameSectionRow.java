7e0e241d2c0ed72b591877318c7c656eb5d0b646
Timothy Redmond <tredmond@stanford.edu>
2011-01-24 12:01:52 +0000

    GForge 1746.  Added the disjoint union axiom frame section.

---
+++ src/org/protege/editor/owl/ui/frame/cls/OWLDisjointUnionAxiomFrameSectionRow.java
@@ -0,0 +1,38 @@
+package org.protege.editor.owl.ui.frame.cls;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.editor.OWLClassExpressionSetEditor;
+import org.protege.editor.owl.ui.editor.OWLObjectEditor;
+import org.protege.editor.owl.ui.frame.AbstractOWLFrameSectionRow;
+import org.protege.editor.owl.ui.frame.OWLFrameSection;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDisjointUnionAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public class OWLDisjointUnionAxiomFrameSectionRow extends AbstractOWLFrameSectionRow<OWLClass, OWLDisjointUnionAxiom, Set<OWLClassExpression>> {
+
+	public OWLDisjointUnionAxiomFrameSectionRow(OWLEditorKit owlEditorKit, OWLFrameSection section,
+												OWLOntology ontology, OWLClass rootObject,
+												OWLDisjointUnionAxiom axiom) {
+		super(owlEditorKit, section, ontology, rootObject, axiom);
+	}
+
+	public List<OWLClassExpression> getManipulatableObjects() {
+		return new ArrayList<OWLClassExpression>(getAxiom().getClassExpressions());
+	}
+
+	@Override
+	protected OWLObjectEditor<Set<OWLClassExpression>> getObjectEditor() {
+		return new OWLClassExpressionSetEditor(getOWLEditorKit(), getManipulatableObjects());
+	}
+
+	@Override
+	protected OWLDisjointUnionAxiom createAxiom(Set<OWLClassExpression> editedObject) {
+		return getOWLDataFactory().getOWLDisjointUnionAxiom(getRootObject(), editedObject);
+	}
+}
