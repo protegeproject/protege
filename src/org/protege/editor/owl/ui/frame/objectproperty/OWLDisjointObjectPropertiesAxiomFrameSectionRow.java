e3d82e2c62fbd9130a0d911c6e38522f80e9859e
Timothy Redmond <tredmond@stanford.edu>
2011-02-13 07:31:38 +0000

    editor handels a set of object property expressions rather than a set of object properties.

--- src/org/protege/editor/owl/ui/frame/objectproperty/OWLDisjointObjectPropertiesAxiomFrameSectionRow.java
+++ src/org/protege/editor/owl/ui/frame/objectproperty/OWLDisjointObjectPropertiesAxiomFrameSectionRow.java
@@ -2,7 +2,7 @@ package org.protege.editor.owl.ui.frame.objectproperty;
 
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.editor.OWLObjectEditor;
-import org.protege.editor.owl.ui.editor.OWLObjectPropertySetEditor;
+import org.protege.editor.owl.ui.editor.OWLObjectPropertyTabbedSetEditor;
 import org.protege.editor.owl.ui.frame.AbstractOWLFrameSectionRow;
 import org.protege.editor.owl.ui.frame.OWLFrameSection;
 import org.semanticweb.owlapi.model.OWLDisjointObjectPropertiesAxiom;
@@ -22,42 +22,36 @@ import java.util.Set;
  * Bio-Health Informatics Group<br>
  * Date: 29-Jan-2007<br><br>
  */
-public class OWLDisjointObjectPropertiesAxiomFrameSectionRow extends AbstractOWLFrameSectionRow<OWLObjectProperty, OWLDisjointObjectPropertiesAxiom, Set<OWLObjectProperty>> {
+public class OWLDisjointObjectPropertiesAxiomFrameSectionRow extends AbstractOWLFrameSectionRow<OWLObjectProperty, OWLDisjointObjectPropertiesAxiom, Set<OWLObjectPropertyExpression>> {
 
 
-    public OWLDisjointObjectPropertiesAxiomFrameSectionRow(OWLEditorKit owlEditorKit, OWLFrameSection section,
+    public OWLDisjointObjectPropertiesAxiomFrameSectionRow(OWLEditorKit owlEditorKit, 
+    													   OWLFrameSection<OWLObjectProperty, OWLDisjointObjectPropertiesAxiom, Set<OWLObjectPropertyExpression>> section,
                                                            OWLOntology ontology, OWLObjectProperty rootObject,
                                                            OWLDisjointObjectPropertiesAxiom axiom) {
         super(owlEditorKit, section, ontology, rootObject, axiom);
     }
 
 
-    protected OWLObjectEditor<Set<OWLObjectProperty>> getObjectEditor() {
-        OWLObjectPropertySetEditor editor = new OWLObjectPropertySetEditor(getOWLEditorKit());
-        final Set<OWLObjectPropertyExpression> disjoints = getAxiom().getProperties();
-        final Set<OWLObjectProperty> namedDisjoints = new HashSet<OWLObjectProperty>();
-        for (OWLObjectPropertyExpression p : disjoints){
-            if (!p.isAnonymous()){
-                namedDisjoints.add(p.asOWLObjectProperty());
-            }
-        }
-        namedDisjoints.remove(getRootObject());
-        editor.setEditedObject(namedDisjoints);
-        // @@TODO handle property expressions
+    protected OWLObjectEditor<Set<OWLObjectPropertyExpression>> getObjectEditor() {
+        OWLObjectPropertyTabbedSetEditor editor = new OWLObjectPropertyTabbedSetEditor(getOWLEditorKit());
+        final Set<OWLObjectPropertyExpression> disjoints = new HashSet<OWLObjectPropertyExpression>(getAxiom().getProperties());
+        disjoints.remove(getRootObject());
+        editor.setEditedObject(disjoints);
         return editor;
     }
 
 
-    protected OWLDisjointObjectPropertiesAxiom createAxiom(Set<OWLObjectProperty> editedObject) {
-        Set<OWLObjectProperty> props = new HashSet<OWLObjectProperty>();
+    protected OWLDisjointObjectPropertiesAxiom createAxiom(Set<OWLObjectPropertyExpression> editedObject) {
+        Set<OWLObjectPropertyExpression> props = new HashSet<OWLObjectPropertyExpression>();
         props.add(getRootObject());
         props.addAll(editedObject);
         return getOWLDataFactory().getOWLDisjointObjectPropertiesAxiom(props);
     }
 
     @Override
-    public boolean checkEditorResults(OWLObjectEditor<Set<OWLObjectProperty>> editor) {
-    	Set<OWLObjectProperty> equivalents = editor.getEditedObject();
+    public boolean checkEditorResults(OWLObjectEditor<Set<OWLObjectPropertyExpression>> editor) {
+    	Set<OWLObjectPropertyExpression> equivalents = editor.getEditedObject();
     	return !equivalents.contains(getRootObject());
     }
 
