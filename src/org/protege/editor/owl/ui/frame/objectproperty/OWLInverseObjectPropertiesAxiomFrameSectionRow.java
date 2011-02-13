6ff7f57ff4131ef2ed7e9fae71bfecddd4e114c2
Timothy Redmond <tredmond@stanford.edu>
2011-02-13 07:34:56 +0000

    imports

--- src/org/protege/editor/owl/ui/frame/objectproperty/OWLInverseObjectPropertiesAxiomFrameSectionRow.java
+++ src/org/protege/editor/owl/ui/frame/objectproperty/OWLInverseObjectPropertiesAxiomFrameSectionRow.java
@@ -1,16 +1,20 @@
 package org.protege.editor.owl.ui.frame.objectproperty;
 
-import org.protege.editor.owl.OWLEditorKit;
-import org.protege.editor.owl.ui.editor.OWLObjectEditor;
-import org.protege.editor.owl.ui.editor.OWLObjectPropertyEditor;
-import org.protege.editor.owl.ui.frame.AbstractOWLFrameSectionRow;
-import org.protege.editor.owl.ui.frame.OWLFrameSection;
-import org.semanticweb.owlapi.model.*;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.editor.OWLObjectEditor;
+import org.protege.editor.owl.ui.editor.OWLObjectPropertyEditor;
+import org.protege.editor.owl.ui.editor.OWLObjectPropertyExpressionEditor;
+import org.protege.editor.owl.ui.frame.AbstractOWLFrameSectionRow;
+import org.protege.editor.owl.ui.frame.OWLFrameSection;
+import org.semanticweb.owlapi.model.OWLInverseObjectPropertiesAxiom;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 
 /**
