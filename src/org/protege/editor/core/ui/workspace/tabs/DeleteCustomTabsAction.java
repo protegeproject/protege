1a190a658ea06a70da59973c179045e31edf588f
Timothy Redmond <tredmond@stanford.edu>
2011-02-14 20:00:43 +0000

    Now they disable themselves if they are not relevant.

--- src/org/protege/editor/core/ui/workspace/tabs/DeleteCustomTabsAction.java
+++ src/org/protege/editor/core/ui/workspace/tabs/DeleteCustomTabsAction.java
@@ -2,8 +2,11 @@ package org.protege.editor.core.ui.workspace.tabs;
 
 import java.awt.event.ActionEvent;
 
-import javax.swing.JMenuItem;
+import javax.swing.JComponent;
+import javax.swing.JMenu;
 import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 import org.protege.editor.core.ui.action.ProtegeAction;
 import org.protege.editor.core.ui.util.JOptionPaneEx;
@@ -13,13 +16,41 @@ import org.protege.editor.core.ui.workspace.WorkspaceTabPlugin;
 
 public class DeleteCustomTabsAction extends ProtegeAction {
 	private static final long serialVersionUID = 5921291642080458478L;
+	
+	private JMenu parentMenu;
+	private MenuListener listener = new MenuListener() {
+		
+		public void menuSelected(MenuEvent e) {
+			updateEnabledStatus();
+		}
+		
+		public void menuDeselected(MenuEvent e) { }
+
+		public void menuCanceled(MenuEvent e) { }
+	};
 
 	public void initialise() throws Exception {
 	}
 
 	public void dispose() throws Exception {
+		if (parentMenu != null) {
+			parentMenu.removeMenuListener(listener);
+			parentMenu = null;
+		}
 	}
 	
+	public void setMenuParent(JComponent parent) {
+		if (parent instanceof JMenu) {
+			parentMenu = (JMenu) parent;
+			parentMenu.addMenuListener(listener);
+		}
+	}
+	
+	
+	private void updateEnabledStatus() {
+		TabbedWorkspace workspace = (TabbedWorkspace) getWorkspace();
+		setEnabled(!workspace.getCustomTabsManager().getCustomTabs().isEmpty());
+	}
 
 	public void actionPerformed(ActionEvent event) {
 		TabbedWorkspace workspace = (TabbedWorkspace) getWorkspace();
