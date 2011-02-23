ae9dec8d0a1e92cb760b578c8905eb29705b7ea8
Timothy Redmond <tredmond@stanford.edu>
2011-02-23 00:04:03 +0000

    renamed the editor to reflect the fact that there is an alternative iri editor.

--- src/org/protege/editor/owl/ui/editor/IRIFromEntityEditor.java
+++ src/org/protege/editor/owl/ui/editor/IRIFromEntityEditor.java
@@ -1,35 +1,17 @@
 package org.protege.editor.owl.ui.editor;
 
-import org.protege.editor.owl.OWLEditorKit;
-import org.protege.editor.owl.ui.selector.OWLEntitySelectorPanel;
-import org.semanticweb.owlapi.model.*;
-
-import javax.swing.*;
-import javax.swing.event.ChangeListener;
 import java.util.Collections;
 import java.util.Set;
-/*
- * Copyright (C) 2007, University of Manchester
- *
- * Modifications to the initial code base are copyright of their
- * respective authors, or their employers as appropriate.  Authorship
- * of the modifications may be determined from the ChangeLog placed at
- * the end of this file.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+import javax.swing.JComponent;
+import javax.swing.event.ChangeListener;
 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.selector.OWLEntitySelectorPanel;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 
 /**
@@ -38,7 +20,7 @@ import java.util.Set;
  * Bio-Health Informatics Group<br>
  * Date: 01-Aug-2007<br><br>
  */
-public class IRIAnnotationValueEditor implements OWLObjectEditor<IRI> {
+public class IRIFromEntityEditor implements OWLObjectEditor<IRI> {
 
     private OWLEntitySelectorPanel entitySelectorPanel;
 
@@ -47,19 +29,28 @@ public class IRIAnnotationValueEditor implements OWLObjectEditor<IRI> {
     private OWLObjectEditorHandler<IRI> handler;
 
 
-    public IRIAnnotationValueEditor(OWLEditorKit owlEditorKit) {
+    public IRIFromEntityEditor(OWLEditorKit owlEditorKit) {
         this.eKit = owlEditorKit;
         entitySelectorPanel = new OWLEntitySelectorPanel(owlEditorKit, false);
     }
 
 
     public boolean canEdit(Object object) {
-        return object instanceof OWLNamedIndividual;
+		boolean contained = false;
+    	if (object instanceof IRI) {
+    		for (OWLOntology ontology : eKit.getModelManager().getActiveOntologies()) {
+    			if (ontology.containsEntityInSignature((IRI) object)) {
+    				contained = true;
+    				break;
+    			}
+    		}
+    	}
+        return contained;
     }
 
 
     public boolean isPreferred(Object object) {
-        return object instanceof OWLNamedIndividual;
+        return false;
     }
 
 
@@ -86,7 +77,11 @@ public class IRIAnnotationValueEditor implements OWLObjectEditor<IRI> {
 
     public Set<IRI> getEditedObjects() {
         IRI selObj = getEditedObject();
-        return selObj != null ? Collections.singleton(selObj) : Collections.EMPTY_SET;
+        if (selObj != null) {
+        	return Collections.singleton(selObj);
+        } else {
+        	return Collections.emptySet();
+        }
     }
 
 
