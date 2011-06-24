faeeedb7dbdfb2d42fe7e25760c8a2cacd1dda45
Timothy Redmond <tredmond@stanford.edu>
2011-06-24 21:59:23 +0000

    remove the equinox method of finding the arguments because it doesn't work well with eclipse

--- src/org/protege/editor/core/platform/PlatformArguments.java
+++ src/org/protege/editor/core/platform/PlatformArguments.java
@@ -1,10 +1,7 @@
 package org.protege.editor.core.platform;
 
-import java.lang.reflect.Method;
-
 import org.apache.log4j.Logger;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 
 /*
  * There currently does not seem to be any generic method for obtaining the command
@@ -20,9 +17,6 @@ public class PlatformArguments {
     public static String[] getArguments(BundleContext context) {
         String[] args;
         if ((args = getWrappedArguments()) != null) {
-            return args;
-        }
-        if ((args = getEquinoxArguments(context)) != null) {
             return args;
         }
         return new String[0];
@@ -48,27 +42,6 @@ public class PlatformArguments {
             args[i] = (String) System.getProperty(ARG_PROPERTY + i);
         }
         return args;
-    }
-
-    public static String[] getEquinoxArguments(BundleContext context) {
-        try {
-            ServiceReference ref = context.getServiceReference("org.eclipse.osgi.service.environment.EnvironmentInfo");
-            if (ref != null) { // OSGi based on eclipse equinox
-                Object o = context.getService(ref);
-                if (o != null) {
-                    try {
-                        Method m = o.getClass().getMethod("getCommandLineArgs", new Class[0]);
-                        return (String[]) m.invoke(o);
-                    }
-                    finally {
-                        context.ungetService(ref);
-                    }
-                }
-            }
-        } catch (Throwable t) { // it is not worth throwing an exception here continue processing.
-            log.warn("Error retrieving command line arguments" + t);
-        }
-        return null;
     }
 
 }
