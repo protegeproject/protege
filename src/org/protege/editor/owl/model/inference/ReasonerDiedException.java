25a539ab6dbca0d0d13d86479d2836d12b5ae69f
Timothy Redmond <tredmond@stanford.edu>
2011-08-16 07:38:36 +0000

    added the exception argument to the reasoner died warning.

--- src/org/protege/editor/owl/model/inference/ReasonerDiedException.java
+++ src/org/protege/editor/owl/model/inference/ReasonerDiedException.java
@@ -3,4 +3,7 @@ package org.protege.editor.owl.model.inference;
 public class ReasonerDiedException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
+	public ReasonerDiedException(Throwable t) {
+		super(t);
+	}
 }
