0d60dcc4ad4b40df5a1a462329db483779c1340a
Timothy Redmond <tredmond@stanford.edu>
2011-08-19 23:30:01 +0000

    modified to clean up the OSGi cache on cleanup.  In addition the cleanup procedure can be overridden

--- src/main/java/org/protege/osgi/framework/Launcher.java
+++ src/main/java/org/protege/osgi/framework/Launcher.java
@@ -15,12 +15,14 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 import org.xml.sax.SAXException;
 
 
-public class Launcher {	
+public class Launcher {    
     public static final String ARG_PROPERTY = "command.line.arg.";
     public static final String LAUNCH_LOCATION_PROPERTY = "org.protege.launch.config";
     public static final String PROTEGE_DIR_PROPERTY = "protege.dir";
@@ -30,14 +32,14 @@ public class Launcher {
     private List<DirectoryWithBundles> directories;
     private String     factoryClass;
     private Framework  framework;
+    private File frameworkDir;
 
     
     public Launcher(File config) throws IOException, ParserConfigurationException, SAXException {
         parseConfig(config);
-		locateOSGi();
-		File frameworkDir = new File(System.getProperty("java.io.tmpdir"), "ProtegeCache-" + UUID.randomUUID().toString());
-		frameworkProperties.setProperty("org.osgi.framework.storage", frameworkDir.getCanonicalPath());
-		frameworkDir.deleteOnExit();
+        locateOSGi();
+        frameworkDir = new File(System.getProperty("java.io.tmpdir"), "ProtegeCache-" + UUID.randomUUID().toString());
+        frameworkProperties.setProperty("org.osgi.framework.storage", frameworkDir.getCanonicalPath());
     }
     
     public Framework getFramework() {
@@ -61,23 +63,39 @@ public class Launcher {
     }
     
     private void setSystemProperties(Parser p) {
-		Properties systemProperties = p.getSystemProperties();
+        Properties systemProperties = p.getSystemProperties();
         System.setProperty("org.protege.plugin.dir", p.getPluginDirectory());
-		for (Entry<Object, Object> entry : systemProperties.entrySet()) {
-			System.setProperty((String) entry.getKey(), (String) entry.getValue());
-		}
-	}
+        for (Entry<Object, Object> entry : systemProperties.entrySet()) {
+            System.setProperty((String) entry.getKey(), (String) entry.getValue());
+        }
+    }
 
-    public void start() throws InstantiationException, IllegalAccessException, ClassNotFoundException, BundleException, IOException {
-    	FrameworkFactory factory = (FrameworkFactory) Class.forName(factoryClass).newInstance();
-    	framework = factory.newFramework(frameworkProperties);
-    	framework.start();
-    	BundleContext context = framework.getBundleContext();
-    	List<Bundle> bundles = new ArrayList<Bundle>();
-    	for (DirectoryWithBundles directory : directories) {
-    	    bundles.addAll(installBundles(context, directory.getDirectory(), directory.getBundles()));
-    	}
-    	startBundles(context, bundles);
+    public void start(final boolean exitOnOSGiShutDown) throws InstantiationException, IllegalAccessException, ClassNotFoundException, BundleException, IOException, InterruptedException {
+        FrameworkFactory factory = (FrameworkFactory) Class.forName(factoryClass).newInstance();
+        framework = factory.newFramework(frameworkProperties);
+        framework.start();
+        BundleContext context = framework.getBundleContext();
+        List<Bundle> bundles = new ArrayList<Bundle>();
+        for (DirectoryWithBundles directory : directories) {
+            bundles.addAll(installBundles(context, directory.getDirectory(), directory.getBundles()));
+        }
+        startBundles(context, bundles);
+        Thread shutdownThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    framework.waitForStop(0);
+                    cleanup();
+                    if (exitOnOSGiShutDown) {
+                        System.exit(0);
+                    }
+                }
+                catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            }
+        }, "OSGi Shutdown Thread");
+        shutdownThread.start();
     }
 
     private List<Bundle> installBundles(BundleContext context, String dir, List<String> bundles) throws BundleException {
@@ -88,48 +106,69 @@ public class Launcher {
         else {
             directory = new File(dir);
         }
-    	List<Bundle> core = new ArrayList<Bundle>();
-    	for (String bundleName :  bundles) {
-    		boolean success = false;
-    		try {
-    			core.add(context.installBundle(new File(directory, bundleName).toURI().toString()));
-    			success = true;
-    		}
-    		finally {
-    			if (!success) {
-    				System.out.println("Core Bundle " + bundleName + " failed to install.");
-    			}
-    		}
-    	}
-    	return core;
+        List<Bundle> core = new ArrayList<Bundle>();
+        for (String bundleName :  bundles) {
+            boolean success = false;
+            try {
+                core.add(context.installBundle(new File(directory, bundleName).toURI().toString()));
+                success = true;
+            }
+            finally {
+                if (!success) {
+                    System.out.println("Core Bundle " + bundleName + " failed to install.");
+                }
+            }
+        }
+        return core;
     }
     
     private void startBundles(BundleContext context, List<Bundle> bundles) throws BundleException {
-    	for (Bundle b : bundles) {
-    		boolean success = false;
-    		try {
-    			b.start();
-    			success = true;
-    		}
-    		finally {
-    			if (!success) {
-    				System.out.println("Core Bundle " + b.getBundleId() + " failed to start.");
-    			}
-    		}
-    	}
+        for (Bundle b : bundles) {
+            boolean success = false;
+            try {
+                b.start();
+                success = true;
+            }
+            finally {
+                if (!success) {
+                    System.out.println("Core Bundle " + b.getBundleId() + " failed to start.");
+                }
+            }
+        }
     }
     
+    protected void cleanup() {
+        delete(frameworkDir);
+    }
 
-	/**
-	 * @param args
-	 * @throws SAXException 
-	 * @throws ParserConfigurationException 
-	 * @throws IOException 
-	 * @throws BundleException 
-	 * @throws ClassNotFoundException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 */
+    private void delete(File f) {
+        if (f.isDirectory()) {
+            for (File child : f.listFiles()) {
+                delete(child);
+            }
+        }
+        f.delete();
+    }
+    
+    public static void setArguments(String... args) {
+        if (args != null) {
+            int counter = 0;
+            for (String arg : args) {
+                System.setProperty(ARG_PROPERTY + (counter++), arg);
+            }
+        }        
+    }
+    
+    /**
+    ddd * @param args
+     * @throws SAXException 
+     * @throws ParserConfigurationException 
+     * @throws IOException 
+     * @throws BundleException 
+     * @throws ClassNotFoundException 
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     */
     public static void main(String[] args) throws Exception {
         setArguments(args);
         String config = System.getProperty(LAUNCH_LOCATION_PROPERTY, "config.xml");
@@ -140,16 +179,8 @@ public class Launcher {
         else {
             configFile = new File(config);
         }
-        new Launcher(configFile).start();
+        Launcher launcher = new Launcher(configFile);
+        launcher.start(true);
     }
     
-    public static void setArguments(String... args) {
-		if (args != null) {
-			int counter = 0;
-			for (String arg : args) {
-				System.setProperty(ARG_PROPERTY + (counter++), arg);
-			}
-		}    	
-    }
-	
 }
