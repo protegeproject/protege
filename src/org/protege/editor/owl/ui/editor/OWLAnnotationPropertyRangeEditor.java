a52fae7351582825d282eab203f9c6d013bfd589
Timothy Redmond <tredmond@stanford.edu>
2011-08-23 17:32:45 +0000

    an annotation property domain and range editor which only allows users to select entities that make sense

---
+++ src/org/protege/editor/owl/ui/editor/OWLAnnotationPropertyRangeEditor.java
@@ -0,0 +1,115 @@
+package org.protege.editor.owl.ui.editor;
+
+import java.awt.Component;
+
+import javax.swing.JComponent;
+import javax.swing.JTabbedPane;
+
+import org.protege.editor.owl.OWLEditorKit;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public class OWLAnnotationPropertyRangeEditor extends AbstractOWLObjectEditor<IRI> {
+    private OWLEditorKit editorKit;
+    private JTabbedPane tabs;
+    private OWLClassSelectorWrapper classSelectionEditor;
+    private OWLDatatypeSelectionEditor datatypeSelectionEditor;
+    private IRITextEditor iriTextEditor;
+    
+    public OWLAnnotationPropertyRangeEditor(OWLEditorKit editorKit) {
+        this.editorKit = editorKit;
+        
+        classSelectionEditor = new OWLClassSelectorWrapper();
+        classSelectionEditor.setup("class.hierarchy", "Class Hierarchy Selection Editor", editorKit);
+        classSelectionEditor.initialise();
+        
+        datatypeSelectionEditor = new OWLDatatypeSelectionEditor(editorKit);
+        
+        iriTextEditor = new IRITextEditor(editorKit);
+        
+        tabs = new JTabbedPane();
+        tabs.add("Select Class", classSelectionEditor.getComponent());
+        tabs.add("Select datatype", datatypeSelectionEditor.getEditorComponent());
+        tabs.add("Edit raw IRI", iriTextEditor.getEditorComponent());
+    }
+
+    public String getEditorTypeName() {
+        return "Annotation Property domain Editor";
+    }
+
+    public boolean canEdit(Object object) {
+        return object instanceof OWLAnnotationProperty;
+    }
+
+    public JComponent getEditorComponent() {
+        return tabs;
+    }
+
+    public IRI getEditedObject() {
+        Component component = tabs.getSelectedComponent();
+        if (component == classSelectionEditor.getComponent()) {
+            for (OWLClassExpression ce : classSelectionEditor.getClassExpressions()) {
+                if (!ce.isAnonymous()) {
+                    return ce.asOWLClass().getIRI();
+                }
+            }
+        }
+        else if (component == datatypeSelectionEditor.getEditorComponent()) {
+            OWLDatatype dt = datatypeSelectionEditor.getEditedObject();
+            return dt != null ? dt.getIRI() : null;
+        }
+        else if (component == iriTextEditor.getEditorComponent()) {
+            return iriTextEditor.getEditedObject();
+        }
+        return null;
+    }
+
+    public boolean setEditedObject(IRI editedObject) {
+        if (editedObject == null) {
+            return false;
+        }
+        OWLDataFactory factory = editorKit.getOWLModelManager().getOWLDataFactory();
+        OWLClass cls = factory.getOWLClass(editedObject);
+        OWLDatatype dt = factory.getOWLDatatype(editedObject);
+        
+        boolean classFound = cls.isBuiltIn();
+        boolean datatypeFound = dt.isBuiltIn();
+        
+        for (OWLOntology o : editorKit.getOWLModelManager().getActiveOntologies()) {
+            if (o.containsEntityInSignature(cls)) {
+                classFound = true;
+            }
+            if (o.containsEntityInSignature(dt)) {
+                datatypeFound = true;
+            }
+            if (classFound && datatypeFound) {
+                break;
+            }
+        }
+        if (classFound) {
+            classSelectionEditor.setDescription(cls);
+        }
+        if (datatypeFound) {
+            datatypeSelectionEditor.setEditedObject(dt);
+        }
+        iriTextEditor.setEditedObject(editedObject);
+        return true;
+    }
+
+    public void dispose() {
+        if (classSelectionEditor != null) { // protect against a double dispose!
+            classSelectionEditor.dispose();
+            iriTextEditor.dispose();
+            datatypeSelectionEditor.dispose();
+            classSelectionEditor = null;
+            iriTextEditor = null;
+            datatypeSelectionEditor = null;
+        }
+    }
+
+}
