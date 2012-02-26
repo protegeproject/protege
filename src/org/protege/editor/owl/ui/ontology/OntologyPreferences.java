514648c64b28b1aa77deec8c1e102d535bacc31f
Matthew Horridge <matthew.horridge@stanford.edu>
2012-02-26 23:15:55 +0000

    Modified the creation of new empty ontologies so that an IRI is assigned to them.

--- src/org/protege/editor/owl/ui/ontology/OntologyPreferences.java
+++ src/org/protege/editor/owl/ui/ontology/OntologyPreferences.java
@@ -19,7 +19,7 @@ import org.protege.editor.core.prefs.PreferencesManager;
 public class OntologyPreferences {
 
 
-    public static final String DEFAULT_BASE = "http://www.semanticweb.org/ontologies";
+    public static final String DEFAULT_BASE = "http://www.semanticweb.org";
 
     public static final String PREFERENCES_KEY = "OntologyPreferences";
 
@@ -30,24 +30,39 @@ public class OntologyPreferences {
     public static final String INC_MONTH_KEY = "INC_MONTH";
 
     public static final String INC_DAY_KEY = "INC_DAY";
+    
+    public static final String INC_DOCUMENT_COUNTER = "INC_DOCUMENT_COUNTER";
+    
+    public static final String DOCUMENT_COUNTER = "DOCUMENT_COUNTER";
 
     private static OntologyPreferences instance;
+
+    public static final String SYSTEM_USER_NAME_PROPERTY = "user.name";
+
+    public static final int DOCUMENT_COUNTER_START = 1;
 
 
     private URI baseURI;
 
-    private boolean includeYear;
+    private boolean includeYear = false;
 
-    private boolean includeMonth;
+    private boolean includeMonth = false;
 
-    private boolean includeDay;
+    private boolean includeDay = false;
+    
+    private boolean useCounter = true;
 
 
     private OntologyPreferences() {
-        baseURI = URI.create(DEFAULT_BASE);
-        includeYear = true;
-        includeMonth = true;
-        includeDay = false;
+        StringBuilder sb = new StringBuilder();
+        sb.append(DEFAULT_BASE);
+        String userName = System.getProperty(SYSTEM_USER_NAME_PROPERTY);
+        if(userName != null) {
+            sb.append("/");
+            sb.append(userName);
+        }
+        sb.append("/ontologies");
+        baseURI = URI.create(sb.toString());
     }
 
 
@@ -61,11 +76,12 @@ public class OntologyPreferences {
 
 
     private void restore() {
-        Preferences p = PreferencesManager.getInstance().getApplicationPreferences(PREFERENCES_KEY);
-        baseURI = URI.create(p.getString(BASE_URI_KEY, DEFAULT_BASE));
+        Preferences p = getPreferences();
+        baseURI = URI.create(p.getString(BASE_URI_KEY, baseURI.toString()));
         includeYear = p.getBoolean(INC_YEAR_KEY, true);
         includeMonth = p.getBoolean(INC_MONTH_KEY, true);
         includeDay = p.getBoolean(INC_DAY_KEY, false);
+        useCounter = p.getBoolean(INC_DOCUMENT_COUNTER, true);
     }
 
 
@@ -114,13 +130,38 @@ public class OntologyPreferences {
 
 
     private void save() {
-        Preferences p = PreferencesManager.getInstance().getApplicationPreferences(PREFERENCES_KEY);
+        Preferences p = getPreferences();
         p.putString(BASE_URI_KEY, baseURI.toString());
         p.putBoolean(INC_YEAR_KEY, includeYear);
         p.putBoolean(INC_MONTH_KEY, includeMonth);
         p.putBoolean(INC_DAY_KEY, includeDay);
     }
 
+    private Preferences getPreferences() {
+        return PreferencesManager.getInstance().getApplicationPreferences(PREFERENCES_KEY);
+    }
+
+    public int getCounter() {
+        return getPreferences().getInt(DOCUMENT_COUNTER, DOCUMENT_COUNTER_START);
+    }
+    
+    public void incrementCounter() {
+        Preferences preferences = getPreferences();
+        int counter = preferences.getInt(DOCUMENT_COUNTER, DOCUMENT_COUNTER_START);
+        counter++;
+        preferences.putInt(DOCUMENT_COUNTER, counter);
+        save();
+    }
+
+    public void resetDocumentCounter() {
+        getPreferences().putInt(DOCUMENT_COUNTER, DOCUMENT_COUNTER_START);
+        save();
+    }
+
+    public URI generateNextURI() {
+        incrementCounter();
+        return generateURI();
+    }
 
     public URI generateURI() {
         String uriString = baseURI.toString();
@@ -136,8 +177,8 @@ public class OntologyPreferences {
                 }
             }
         }
-        String name = "Ontology" + System.currentTimeMillis() + ".owl";
-        uriString += name;
+        String documentName = "untitled-ontology-" + getCounter();
+        uriString += documentName;
         return URI.create(uriString);
     }
 }
