1d580767422b0d7a810f52853f20c87bc837e3a8
Timothy Redmond <tredmond@stanford.edu>
2012-06-19 18:28:01 +0000

    an action to stop inference.  I know that several people, including myself, stop a reasoner by selecting another reasoner type and then selecting my reasoner type back.

---
+++ src/org/protege/editor/owl/ui/inference/StopReasonerAction.java
@@ -0,0 +1,29 @@
+package org.protege.editor.owl.ui.inference;
+
+import java.awt.event.ActionEvent;
+
+import org.protege.editor.owl.model.event.EventType;
+import org.protege.editor.owl.model.inference.OWLReasonerManager;
+import org.protege.editor.owl.ui.action.ProtegeOWLAction;
+
+public class StopReasonerAction extends ProtegeOWLAction {
+	private static final long serialVersionUID = 3727652674495540747L;
+
+	@Override
+	public void initialise() throws Exception {
+		// is not called!
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		OWLReasonerManager reasonerManager = getOWLModelManager().getOWLReasonerManager();
+		reasonerManager.killCurrentReasoner();
+		getOWLModelManager().fireEvent(EventType.REASONER_CHANGED);
+	}
+
+	@Override
+	public void dispose() throws Exception {
+
+	}
+
+}
