0070f8335e809f140d8b864031adc95dad50d532
Matthew Horridge <matthew.horridge@stanford.edu>
2012-11-07 00:23:58 +0000

    Modified the OWLIcons utility class to search for icon files in an icons directory as well as the root directory of the bundle.

--- src/org/protege/editor/owl/ui/OWLIcons.java
+++ src/org/protege/editor/owl/ui/OWLIcons.java
@@ -20,28 +20,60 @@ public class OWLIcons {
 
     private static HashMap<String, Icon> iconMap;
 
+    public static final String ALTERNATIVE_ICONS_DIRECTORY = "icons/";
+
 
     static {
         iconMap = new HashMap<String, Icon>();
     }
 
-
-    public static Icon getIcon(String name) {
-        Icon icon = iconMap.get(name);
+    /**
+     * Gets the Icon with the specified file name.
+     * @param fileName The file name.  May be {@code null}.
+     * @return The Icon that is depicted by the specified file name, or {@code null} if there is no file in the class
+     * path with the specified file name, of {@code fileName} is {@code null}.
+     */
+    public static Icon getIcon(String fileName) {
+        if(fileName == null) {
+            return null;
+        }
+        Icon icon = iconMap.get(fileName);
         if (icon != null) {
             return icon;
         }
-        else {
-            ClassLoader loader = OWLIcons.class.getClassLoader();
-            URL url = loader.getResource(name);
-            if (url != null) {
-                Icon loadedIcon = new ImageIcon(url);
-                iconMap.put(name, loadedIcon);
-                return loadedIcon;
-            }
-            else {
-                return null;
-            }
+        URL url = getIconURL(fileName);
+        if (url == null) {
+            return null;
         }
+        Icon loadedIcon = new ImageIcon(url);
+        iconMap.put(fileName, loadedIcon);
+        return loadedIcon;
+    }
+
+    /**
+     * Gets the URL of the icon file denoted by the specified file name.
+     * @param fileName The file name.  May be {@code null}.
+     * @return The URL of the specified file name, or {@code null} if {@code fileName} is {@code null}, or {@code null}
+     *         if there is not file with the specified file name in the bundle class path.
+     */
+    private static URL getIconURL(String fileName) {
+        if (fileName == null) {
+            return null;
+        }
+        ClassLoader loader = OWLIcons.class.getClassLoader();
+        URL url = loader.getResource(fileName);
+        if (url == null && !isFileNameAbsolute(fileName)) {
+            url = loader.getResource(ALTERNATIVE_ICONS_DIRECTORY + fileName);
+        }
+        return url;
+    }
+
+    /**
+     * Determines if the specified file name is absolute.
+     * @param fileName The file name.
+     * @return {@code true} if the specified file name is absolute, otherwise {@code false}.
+     */
+    private static boolean isFileNameAbsolute(String fileName) {
+        return fileName.startsWith("/");
     }
 }
