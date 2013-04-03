0d97f2bb6fcb3e9afa42c4305525209e6e1fbd15
Jennifer Vendetti <vendetti@stanford.edu>
2013-04-03 20:28:45 +0000

    cleaned up unused import warnings

--- src/main/java/org/protege/osgi/framework/Parser.java
+++ src/main/java/org/protege/osgi/framework/Parser.java
@@ -3,7 +3,6 @@ package org.protege.osgi.framework;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
