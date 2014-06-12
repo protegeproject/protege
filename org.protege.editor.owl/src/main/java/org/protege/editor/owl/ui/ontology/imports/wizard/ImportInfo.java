87d1e065bb2176e10addf62412e0c9fa8558103f
Matthew Horridge <matthew.horridge@stanford.edu>
2014-06-12 12:21:28 -0700

    Fixes #57

--- org.protege.editor.owl/src/main/java/org/protege/editor/owl/ui/ontology/imports/wizard/ImportInfo.java
+++ org.protege.editor.owl/src/main/java/org/protege/editor/owl/ui/ontology/imports/wizard/ImportInfo.java
@@ -1,6 +1,7 @@
 package org.protege.editor.owl.ui.ontology.imports.wizard;
 
 import java.net.URI;
+
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 
@@ -15,30 +16,56 @@ import org.semanticweb.owlapi.model.OWLOntologyID;
  * www.cs.man.ac.uk/~horridgm<br><br>
  */
 public class ImportInfo {
-	private OWLOntologyID ontologyID;
-	private URI physicalLocation;
-	private IRI importLocation;
-	
-	public OWLOntologyID getOntologyID() {
-		return ontologyID;
-	}
-	public void setOntologyID(OWLOntologyID ontologyID) {
-		this.ontologyID = ontologyID;
-	}
-	public URI getPhysicalLocation() {
-		return physicalLocation;
-	}
-	public void setPhysicalLocation(URI physicalLocation) {
-		this.physicalLocation = physicalLocation;
-	}
-	public IRI getImportLocation() {
-		return importLocation;
-	}
-	public void setImportLocation(IRI importLocation) {
-		this.importLocation = importLocation;
-	}
-	
-	public boolean isReady() {
-		return importLocation != null && ontologyID != null & physicalLocation != null;
-	}
+
+    private OWLOntologyID ontologyID;
+
+    private URI physicalLocation;
+
+    private IRI importLocation;
+
+    public OWLOntologyID getOntologyID() {
+        return ontologyID;
+    }
+
+    public void setOntologyID(OWLOntologyID ontologyID) {
+        this.ontologyID = ontologyID;
+    }
+
+    public URI getPhysicalLocation() {
+        return physicalLocation;
+    }
+
+    public void setPhysicalLocation(URI physicalLocation) {
+        this.physicalLocation = physicalLocation;
+    }
+
+    public IRI getImportLocation() {
+        return importLocation;
+    }
+
+    public void setImportLocation(IRI importLocation) {
+        this.importLocation = importLocation;
+    }
+
+    public boolean isReady() {
+        return importLocation != null && ontologyID != null & physicalLocation != null;
+    }
+
+    /**
+     * Gets the IRI that should be used as the target of the imports declaration.
+     * @return  The IRI.  Not {@code null}.
+     * @throws IllegalStateException if this methods is called and {@link #isReady()} returns false.
+     */
+    public IRI getImportsDeclarationIRI() {
+        if(!isReady()) {
+            throw new IllegalStateException("ImportInfo is not ready.");
+        }
+        if(ontologyID != null) {
+            IRI defaultDocumentIRI = ontologyID.getDefaultDocumentIRI();
+            if(defaultDocumentIRI != null) {
+                return defaultDocumentIRI;
+            }
+        }
+        return importLocation;
+    }
 }
