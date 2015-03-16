0ff559e6cd806970c73f6ca2eb980eda0dd46d44
Matthew Horridge <matthew.horridge@stanford.edu>
2015-03-16 16:45:36 -0700

    Closes #159

--- protege-distribution/src/main/scripts/nix/run.sh
+++ protege-distribution/src/main/scripts/nix/run.sh
@@ -6,7 +6,7 @@ java -Xmx500M -Xms250M \
      -Dlog4j.configuration=file:log4j.xml \
      -DentityExpansionLimit=100000000 \
      -Dfile.encoding=UTF-8 \
-     -classpath bin/felix.jar:bin/ProtegeLauncher.jar \
+     -classpath bin/org.apache.felix.main.jar:bin/ProtegeLauncher.jar \
      $CMD_OPTIONS org.protege.osgi.framework.Launcher
 
 
