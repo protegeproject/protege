5be2b1a9d73226d72a679ee552eb3d279e44460f
Matthew Horridge <matthew.horridge@stanford.edu>
2015-08-31 16:34:59 -0700

    Fixes #231

---
+++ org.protege.editor.owl/src/main/java/org/protege/editor/owl/ui/view/HasExpandAll.java
@@ -0,0 +1,13 @@
+package org.protege.editor.owl.ui.view;
+
+import org.protege.editor.owl.ui.action.ActionTarget;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 31/08/15
+ */
+public interface HasExpandAll extends ActionTarget {
+
+    void expandAll();
+}
