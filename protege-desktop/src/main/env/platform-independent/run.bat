e64022b12e0619a188845d0780b60531db206b3b
Matthew Horridge <matthew.horridge@stanford.edu>
2015-09-08 17:58:01 -0700

    Upgraded to OWL API 3.5.3.  Removed plugins from build of app
    - Upgraded to OWL API 3.5.3.  Closes #230.
    - Removed plugins from the old distribution module.  Closes #238.
    - Renamed modules to have more informative names (these no longer
    correspond to the symbolic bundle names, but I don’t think that’s an
    issue).
    - Tidied up artifact ids (Closes #240 and #242)

---
+++ protege-desktop/src/main/env/platform-independent/run.bat
@@ -0,0 +1,2 @@
+
+java -Dlog4j.configuration=file:log4j.xml -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bin/org.apache.felix.main.jar;bin/protege-launcher.jar org.protege.osgi.framework.Launcher %1
--- protege-distribution/src/main/env/platform-independent/run.bat
+++
@@ -1,2 +0,0 @@
-
-java -Dlog4j.configuration=file:log4j.xml -DentityExpansionLimit=100000000 -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bin/org.apache.felix.main.jar;bin/ProtegeLauncher.jar org.protege.osgi.framework.Launcher %1
