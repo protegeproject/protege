bf7785508fb7b6054488ba93d8521012f3ae7180
Matthew Horridge <matthew.horridge@stanford.edu>
2015-09-11 15:47:21 -0700

    Added missing guava dependency to classpath in run.sh for Linux

--- protege-desktop/src/main/env/linux/run.sh
+++ protege-desktop/src/main/env/linux/run.sh
@@ -6,7 +6,7 @@ jre/bin/java -Xmx${conf.mem.xmx} -Xms${conf.mem.xms} \
      -Dlog4j.configuration=${conf.log4j.location} \
      -DentityExpansionLimit=100000000 \
      -Dfile.encoding=UTF-8 \
-     -classpath bin/org.apache.felix.main.jar:bin/protege-launcher.jar \
+     -classpath bin/guava.jar:bin/org.apache.felix.main.jar:bin/protege-launcher.jar \
      $CMD_OPTIONS org.protege.osgi.framework.Launcher $1
 
 
