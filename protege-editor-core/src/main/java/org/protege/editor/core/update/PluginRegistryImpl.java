b98da9ac9022bb863c7b8d9824d4f8834ffcd7ee
Matthew Horridge <matthew.horridge@stanford.edu>
2015-11-16 15:56:28 -0800

    Added some indentation to logging output

--- protege-editor-core/src/main/java/org/protege/editor/core/update/PluginRegistryImpl.java
+++ protege-editor-core/src/main/java/org/protege/editor/core/update/PluginRegistryImpl.java
@@ -88,7 +88,7 @@ public class PluginRegistryImpl implements PluginRegistry {
         public void run() {
             logger.info(LogBanner.start("Running Auto-update"));
             checkBundles();
-            visit(root);
+            visit(root, 0);
             sortPlugins();
             logger.info(LogBanner.end());
         }
@@ -132,22 +132,22 @@ public class PluginRegistryImpl implements PluginRegistry {
             }
         }
         
-        private void visit(URL node) {
+        private void visit(URL node, int depth) {
             if (!visitedURLs.contains(node)){
                 visitedURLs.add(node);
                 
-                logger.info(AUTO_UPDATE, "Checking {}", node);
+                logger.info(AUTO_UPDATE, "{}Checking {}", pad(depth), node);
 
                 // see if this is a plugin file
                 try{
                     UpdateChecker checker = new UpdateChecker(node, null);
                     PluginInfo info = checker.run();
                     if (info != null) {
-                        logger.debug(AUTO_UPDATE, "URL {} has valid plugin info: {}", node, info.getId());
+                        logger.debug(AUTO_UPDATE, "{}URL {} has valid plugin info: {}", pad(depth), node, info.getId());
 
                         if (pluginType == PluginRegistryType.PLUGIN_DOWNLOAD_REGISTRY && !bundleByIds.containsKey(info.getId())) {
                             plugins.add(info);
-                            logger.debug(AUTO_UPDATE, "URL {} is a download", node);
+                            logger.debug(AUTO_UPDATE, "{}URL {} is a download", pad(depth), node);
 
                         }
                         Bundle bundle = bundleByIds.get(info.getId());
@@ -157,52 +157,60 @@ public class PluginRegistryImpl implements PluginRegistry {
                                       && !selfUpdatingBundleIds.contains(info.getId())) {
                             info.setPluginDescriptor(bundle);
                             plugins.add(info);
-                            logger.debug(AUTO_UPDATE, "URL {} is an update", node);
+                            logger.debug(AUTO_UPDATE, "{}URL {} is an update", pad(depth), node);
                         }
                     }
 
                 }
                 catch(MalformedURLException e){
-                    readRegistry(node);
+                    readRegistry(node, depth + 1);
                 }
                 catch (UpdateException e){
                     if (e.getPluginId().isPresent()) {
-                        logger.warn(AUTO_UPDATE, "Couldn't read plugin updated file at {}.  Reason: {}", node, e.getMessage(), e);
+                        logger.warn(AUTO_UPDATE, "{}Couldn't read plugin updated file at {}.  Reason: {}", pad(depth), node, e.getMessage(), e);
                     }
-                    readRegistry(node);
+                    readRegistry(node, depth + 1);
                 }
                 catch(IOException e){
-                    logger.warn(AUTO_UPDATE, "Cannot open remote plugin file/registry at {}.  Reason: {}", node, e.getCause(), e);
+                    logger.warn(AUTO_UPDATE, "{}Cannot open remote plugin file/registry at {}.  Reason: {}", pad(depth), node, e.getCause(), e);
                 }
             }
         }
 
-        private void readRegistry(URL node) {
-            logger.debug(AUTO_UPDATE, "Attempting to process info at {} as a plugin registry", node);
+        private void readRegistry(URL node, int depth) {
+            logger.info(AUTO_UPDATE, "{}Processing {} as a plugin registry", pad(depth), node);
             try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(node.openStream())));
                 String line;
                 while ((line = reader.readLine()) != null) {
                     line = line.trim();
                     if (logger.isDebugEnabled()) {
-                        logger.debug(AUTO_UPDATE, "\treading line from node " + node + ":" + line);
+                        logger.debug(AUTO_UPDATE, "{}   reading line from node " + node + ":" + line, pad(depth));
                     }
                     if (line.length() > 0 && !line.startsWith("//")){
                         try{
                             URL url = new URL(line);
-                            visit(url);
+                            visit(url, depth + 1);
                         }
                         catch(MalformedURLException urlException){
-                            logger.debug(AUTO_UPDATE, "Invalid URL in plugin registry: " + line);
+                            logger.debug(AUTO_UPDATE, "{}    Invalid URL in plugin registry: " + line, pad(depth));
                         }
                     }
                 }
                 reader.close();
             }
             catch (IOException ex) {
-                logger.warn(AUTO_UPDATE, "Cannot open remote plugin registry at {}.  Reason: {}", ex.getMessage(), ex);
+                logger.warn(AUTO_UPDATE, "{}    Cannot open remote plugin registry at {}.  Reason: {}",pad(depth), ex.getMessage(), ex);
             }
         }
+    }
 
+
+    private static String pad(int depth) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < depth; i++) {
+            sb.append("    ");
+        }
+        return sb.toString();
     }
 }
