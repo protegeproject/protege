10fac062092069d4da34e993e3fdb9cd934e5c69
Matthew Horridge <matthew.horridge@stanford.edu>
2015-11-16 11:35:21 -0800

    Add cause to log rendering

--- protege-editor-core/src/main/java/org/protege/editor/core/log/LogRecordRenderer.java
+++ protege-editor-core/src/main/java/org/protege/editor/core/log/LogRecordRenderer.java
@@ -5,6 +5,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Optional;
 
 /**
  * Matthew Horridge
@@ -59,17 +60,35 @@ public class LogRecordRenderer implements ListCellRenderer<LogRecord> {
         sb.append(firstLine);
         sb.append("</span><br>\n");
         ThrowableInfo throwableInfo = record.getThrowableInfo().get();
-        sb.append("                        ");
-        sb.append(throwableInfo.getClassName());
-        sb.append("<br>\n");
-        for(StackTraceElement element : throwableInfo.getStackTrace()) {
-            sb.append("                            ");
-            sb.append(element);
-            sb.append("<br>\n");
-        }
+        renderThrowableInfo(sb, throwableInfo, 0);
         sb.append("</body></html>");
         return sb.toString().replace(" ", "&nbsp;");
 
+    }
+
+    private static void renderThrowableInfo(StringBuilder sb, ThrowableInfo throwableInfo, int depth) {
+        int padding = depth + 1;
+        pad(sb, padding);
+        sb.append(throwableInfo.getClassName());
+        sb.append("<br>\n");
+        for(StackTraceElement element : throwableInfo.getStackTrace()) {
+            pad(sb, padding);
+            sb.append(element);
+            sb.append("<br>\n");
+        }
+        Optional<ThrowableInfo> cause = throwableInfo.getCause();
+        if(cause.isPresent()) {
+            pad(sb, padding);
+            sb.append("Caused by:<br>\n");
+            renderThrowableInfo(sb, cause.get(), depth + 1);
+        }
+    }
+
+    private static void pad(StringBuilder sb, int padding) {
+        sb.append("                ");
+        for(int i = 0; i < padding; i++) {
+            sb.append("        ");
+        }
     }
 
     private String getTimestampRendering(long timestamp) {
