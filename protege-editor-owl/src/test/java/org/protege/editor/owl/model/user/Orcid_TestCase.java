cdd3d29da2b0af93e5d2f89305e15aa0262c42f1
Matthew Horridge <matthew.horridge@stanford.edu>
2015-11-25 16:42:22 -0800

    An object to represent ORCIDs
    plus tests

---
+++ protege-editor-owl/src/test/java/org/protege/editor/owl/model/user/Orcid_TestCase.java
@@ -0,0 +1,71 @@
+
+package org.protege.editor.owl.model.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(org.mockito.runners.MockitoJUnitRunner.class)
+public class Orcid_TestCase {
+
+    private Orcid orcid;
+
+    private String value = "0000-1234-5678-0000";
+
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        orcid = new Orcid(value);
+    }
+
+    @Test(expected = java.lang.NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_value_IsNull() {
+        new Orcid(null);
+    }
+
+    @Test
+    public void shouldReturnSupplied_value() {
+        assertThat(orcid.getValue(), is(this.value));
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(orcid, is(orcid));
+    }
+
+    @Test
+    public void shouldNotBeEqualToNull() {
+        assertThat(orcid.equals(null), is(false));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(orcid, is(new Orcid(value)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_value() {
+        assertThat(orcid, is(not(new Orcid("0000-1234-5678-111X"))));
+    }
+
+    @Test
+    public void shouldBeEqualToOtherHashCode() {
+        assertThat(orcid.hashCode(), is(new Orcid(value).hashCode()));
+    }
+
+    @Test
+    public void shouldImplementToString() {
+        assertThat(orcid.toString(), startsWith("Orcid"));
+    }
+
+    public void shouldGetUri() {
+        assertThat(orcid.toUri(), is(URI.create("http://orcid.org/" + value)));
+    }
+
+}
