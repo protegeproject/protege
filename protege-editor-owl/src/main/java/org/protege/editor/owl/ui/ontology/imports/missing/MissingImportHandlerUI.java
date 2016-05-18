0a90a639e1ff2916fd214e2dd0d0397a3e6d74cb
Matthew Horridge <matthew.horridge@stanford.edu>
2016-05-18 13:57:16 -0700

    Make sure imports resolution happens in the EDT

--- protege-editor-owl/src/main/java/org/protege/editor/owl/ui/ontology/imports/missing/MissingImportHandlerUI.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/ontology/imports/missing/MissingImportHandlerUI.java
@@ -15,6 +15,8 @@ import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 
 /**
@@ -30,7 +32,7 @@ public class MissingImportHandlerUI implements MissingImportHandler {
 
     private final Logger logger = LoggerFactory.getLogger(MissingImportHandlerUI.class);
 
-    private OWLEditorKit owlEditorKit;
+    private final OWLEditorKit owlEditorKit;
 
 
     public MissingImportHandlerUI(OWLEditorKit owlEditorKit) {
@@ -39,26 +41,36 @@ public class MissingImportHandlerUI implements MissingImportHandler {
 
 
     public IRI getDocumentIRI(IRI ontologyIRI) {
+        FutureTask<IRI> futureTask = new FutureTask<>(() -> {
+            int ret = JOptionPane.showConfirmDialog(null,
+                    "<html><body>The system couldn't locate the ontology:<br><font color=\"blue\">" + ontologyIRI.toString() + "</font><br><br>" +
 
-        int ret = JOptionPane.showConfirmDialog(null,
-                                                "<html><body>The system couldn't locate the ontology:<br><font color=\"blue\">" + ontologyIRI.toString() + "</font><br><br>" +
+                            "Would you like to attempt to resolve the missing import?</body></html>",
+                    "Resolve missing import?",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.WARNING_MESSAGE);
+            if (ret != JOptionPane.YES_OPTION) {
+                return null;
+            }
+            UIHelper helper = new UIHelper(owlEditorKit);
+            File file = helper.chooseOWLFile("Please select an ontology file");
+            if (file == null) {
+                return ontologyIRI;
+            }
+            updateActiveCatalog(ontologyIRI, file);
+            return IRI.create(file);
+        });
 
-                                                        "Would you like to attempt to resolve the missing import?</body></html>",
-                                                "Resolve missing import?",
-                                                JOptionPane.YES_NO_OPTION,
-                                                JOptionPane.WARNING_MESSAGE);
-        if (ret != JOptionPane.YES_OPTION) {
+        SwingUtilities.invokeLater(futureTask);
+        try {
+            return futureTask.get();
+        } catch (InterruptedException e) {
+            logger.debug("Resolve import task interrupted");
+            return null;
+        } catch (ExecutionException e) {
+            logger.error("An exception was thrown whilst the user was resolving a missing import: {}", e.getCause().getMessage());
             return null;
         }
-        UIHelper helper = new UIHelper(owlEditorKit);
-        File file = helper.chooseOWLFile("Please select an ontology file");
-        if (file == null) {
-            return ontologyIRI;
-        }
-        updateActiveCatalog(ontologyIRI, file);
-
-        return IRI.create(file);
-
     }
     
     private void updateActiveCatalog(IRI ontologyIRI, File file) {
