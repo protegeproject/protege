b9a0838ee95a1aff24ab8c10bcbb5b2ccf924b44
Dmitry Tsarkov <dmitry.tsarkov@gmail.com>
2016-05-18 23:11:24 +0300

    Fix #434 by increasing default JVM stack size

--- protege-desktop/src/main/env/win/run.bat
+++ protege-desktop/src/main/env/win/run.bat
@@ -1,3 +1,3 @@
 setlocal
 cd /d %~dp0
-jre\bin\java -Xmx${conf.mem.xmx}M -Xms${conf.mem.xms}M ${conf.extra.args} -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback-win.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bundles/guava.jar;bundles/logback-classic.jar;bundles/logback-core.jar;bundles/slf4j-api.jar;bin/org.apache.felix.main.jar;bin/protege-launcher.jar org.protege.osgi.framework.Launcher %1
+jre\bin\java -Xmx${conf.mem.xmx}M -Xms${conf.mem.xms}M -Xss${conf.mem.xss}M ${conf.extra.args} -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback-win.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bundles/guava.jar;bundles/logback-classic.jar;bundles/logback-core.jar;bundles/slf4j-api.jar;bin/org.apache.felix.main.jar;bin/protege-launcher.jar org.protege.osgi.framework.Launcher %1
