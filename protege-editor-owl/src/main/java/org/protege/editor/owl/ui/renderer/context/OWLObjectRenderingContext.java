0ea5504c810af033f12fe3762307a86cec4928f9
Matthew Horridge <matthew.horridge@stanford.edu>
2016-11-07 14:19:29 -0800

    Closes #577

--- protege-editor-owl/src/main/java/org/protege/editor/owl/ui/renderer/context/OWLObjectRenderingContext.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/renderer/context/OWLObjectRenderingContext.java
@@ -1,8 +1,6 @@
 package org.protege.editor.owl.ui.renderer.context;
 
-import org.semanticweb.owlapi.util.OntologyIRIShortFormProvider;
-import org.semanticweb.owlapi.util.ShortFormProvider;
-import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+import org.semanticweb.owlapi.util.*;
 
 /**
  * Author: Matthew Horridge<br>
@@ -12,22 +10,30 @@ import org.semanticweb.owlapi.util.SimpleShortFormProvider;
  */
 public class OWLObjectRenderingContext {
 
-    final private DeprecatedObjectChecker deprecatedObjectChecker;
+    private final DeprecatedObjectChecker deprecatedObjectChecker;
 
-    final private ClassSatisfiabilityChecker classSatisfiabilityChecker;
+    private final ClassSatisfiabilityChecker classSatisfiabilityChecker;
 
-    final private ObjectPropertySatisfiabilityChecker objectPropertySatisfiabilityChecker;
+    private final ObjectPropertySatisfiabilityChecker objectPropertySatisfiabilityChecker;
 
-    final private DataPropertySatisfiabilityChecker dataPropertySatisfiabilityChecker;
+    private final DataPropertySatisfiabilityChecker dataPropertySatisfiabilityChecker;
 
-    final private LinkFactory linkFactory;
+    private final LinkFactory linkFactory;
 
-    final private ShortFormProvider shortFormProvider;
+    private final ShortFormProvider shortFormProvider;
+
+    private final IRIShortFormProvider iriShortFormProvider;
 
     final private OntologyIRIShortFormProvider ontologyIRIShortFormProvider;
 
 
-    public OWLObjectRenderingContext(DeprecatedObjectChecker deprecatedObjectChecker, ClassSatisfiabilityChecker classSatisfiabilityChecker, ObjectPropertySatisfiabilityChecker objectPropertySatisfiabilityChecker, DataPropertySatisfiabilityChecker dataPropertySatisfiabilityChecker, LinkFactory linkFactory, ShortFormProvider shortFormProvider, OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
+    public OWLObjectRenderingContext(DeprecatedObjectChecker deprecatedObjectChecker,
+                                     ClassSatisfiabilityChecker classSatisfiabilityChecker,
+                                     ObjectPropertySatisfiabilityChecker objectPropertySatisfiabilityChecker,
+                                     DataPropertySatisfiabilityChecker dataPropertySatisfiabilityChecker,
+                                     LinkFactory linkFactory,
+                                     ShortFormProvider shortFormProvider,
+                                     OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
         this.deprecatedObjectChecker = deprecatedObjectChecker;
         this.classSatisfiabilityChecker = classSatisfiabilityChecker;
         this.objectPropertySatisfiabilityChecker = objectPropertySatisfiabilityChecker;
@@ -35,12 +41,50 @@ public class OWLObjectRenderingContext {
         this.linkFactory = linkFactory;
         this.shortFormProvider = shortFormProvider;
         this.ontologyIRIShortFormProvider = ontologyIRIShortFormProvider;
+        this.iriShortFormProvider = new SimpleIRIShortFormProvider();
     }
 
-    public OWLObjectRenderingContext(ShortFormProvider shortFormProvider, OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
-        this(new NullDeprecatedObjectChecker(), new NullClassSatisfiabilityChecker(), new NullObjectPropertySatisfiabilityChecker(), new NullDataPropertySatisfiabilityChecker(), new NullLinkFactory(), shortFormProvider, ontologyIRIShortFormProvider);
+    public OWLObjectRenderingContext(DeprecatedObjectChecker deprecatedObjectChecker,
+                                     ClassSatisfiabilityChecker classSatisfiabilityChecker,
+                                     ObjectPropertySatisfiabilityChecker objectPropertySatisfiabilityChecker,
+                                     DataPropertySatisfiabilityChecker dataPropertySatisfiabilityChecker,
+                                     LinkFactory linkFactory,
+                                     ShortFormProvider shortFormProvider,
+                                     IRIShortFormProvider iriShortFormProvider,
+                                     OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
+        this.deprecatedObjectChecker = deprecatedObjectChecker;
+        this.classSatisfiabilityChecker = classSatisfiabilityChecker;
+        this.objectPropertySatisfiabilityChecker = objectPropertySatisfiabilityChecker;
+        this.dataPropertySatisfiabilityChecker = dataPropertySatisfiabilityChecker;
+        this.linkFactory = linkFactory;
+        this.shortFormProvider = shortFormProvider;
+        this.iriShortFormProvider = iriShortFormProvider;
+        this.ontologyIRIShortFormProvider = ontologyIRIShortFormProvider;
     }
 
+    public OWLObjectRenderingContext(ShortFormProvider shortFormProvider,
+                                     OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
+        this(new NullDeprecatedObjectChecker(),
+             new NullClassSatisfiabilityChecker(),
+             new NullObjectPropertySatisfiabilityChecker(),
+             new NullDataPropertySatisfiabilityChecker(),
+             new NullLinkFactory(),
+             shortFormProvider,
+             ontologyIRIShortFormProvider);
+    }
+
+    public OWLObjectRenderingContext(ShortFormProvider shortFormProvider,
+                                     IRIShortFormProvider iriShortFormProvider,
+                                     OntologyIRIShortFormProvider ontologyIRIShortFormProvider) {
+        this(new NullDeprecatedObjectChecker(),
+             new NullClassSatisfiabilityChecker(),
+             new NullObjectPropertySatisfiabilityChecker(),
+             new NullDataPropertySatisfiabilityChecker(),
+             new NullLinkFactory(),
+             shortFormProvider,
+             iriShortFormProvider,
+             ontologyIRIShortFormProvider);
+    }
 
     public OWLObjectRenderingContext() {
         deprecatedObjectChecker = new NullDeprecatedObjectChecker();
@@ -50,6 +94,7 @@ public class OWLObjectRenderingContext {
         linkFactory = new NullLinkFactory();
         shortFormProvider = new SimpleShortFormProvider();
         ontologyIRIShortFormProvider = new OntologyIRIShortFormProvider();
+        iriShortFormProvider = new SimpleIRIShortFormProvider();
     }
 
     public ShortFormProvider getShortFormProvider() {
@@ -66,7 +111,13 @@ public class OWLObjectRenderingContext {
 
 
     public OWLObjectRenderingContext setClassSatisfiabilityChecker(ClassSatisfiabilityChecker classSatisfiabilityChecker) {
-        return new OWLObjectRenderingContext(this.deprecatedObjectChecker, classSatisfiabilityChecker, this.objectPropertySatisfiabilityChecker, this.dataPropertySatisfiabilityChecker, this.linkFactory, this.shortFormProvider, this.ontologyIRIShortFormProvider);
+        return new OWLObjectRenderingContext(this.deprecatedObjectChecker,
+                                             classSatisfiabilityChecker,
+                                             this.objectPropertySatisfiabilityChecker,
+                                             this.dataPropertySatisfiabilityChecker,
+                                             this.linkFactory,
+                                             this.shortFormProvider,
+                                             this.ontologyIRIShortFormProvider);
     }
 
 
@@ -85,5 +136,9 @@ public class OWLObjectRenderingContext {
 
     public LinkFactory getLinkFactory() {
         return linkFactory;
+    }
+
+    public IRIShortFormProvider getIriShortFormProvider() {
+        return iriShortFormProvider;
     }
 }
