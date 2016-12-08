e1cf1aedc49d7fe02f3539e7b88708d3ad525113
Bob Dionne <dionne@dionne-associates.com>
2016-12-08 10:40:28 -0500

    Remove ability to split equivalent classes conjunctions out to separate terms

--- protege-editor-owl/src/main/java/org/protege/editor/owl/ui/framelist/OWLFrameList.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/framelist/OWLFrameList.java
@@ -338,7 +338,7 @@ public class OWLFrameList<R> extends MList implements LinkedObjectComponent, Dro
         addToPopupMenu(new MoveAxiomsToOntologyAction<>());
         addToPopupMenu(new CreateConjunctTermAction<>());
         addToPopupMenu(new CreateDisjunctTermAction<>());
-        addToPopupMenu(new SplitEquivTermsAction<>());
+        //addToPopupMenu(new SplitEquivTermsAction<>());
     }
 
     public void addToPopupMenu(OWLFrameListPopupMenuAction<R> action) {
