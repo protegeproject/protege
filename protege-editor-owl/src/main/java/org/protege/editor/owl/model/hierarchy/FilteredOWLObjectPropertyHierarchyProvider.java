95cdc524a2bd27fdc4a597ecc193251b8464c57a
Bob Dionne <dionne@dionne-associates.com>
2017-03-22 13:09:05 -0400

    Take into account defined classes when searching for domain

--- protege-editor-owl/src/main/java/org/protege/editor/owl/model/hierarchy/FilteredOWLObjectPropertyHierarchyProvider.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/model/hierarchy/FilteredOWLObjectPropertyHierarchyProvider.java
@@ -6,8 +6,28 @@ import java.util.Set;
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.model.selection.OWLSelectionModelListener;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLClassExpressionVisitor;
+import org.semanticweb.owlapi.model.OWLDataAllValuesFrom;
+import org.semanticweb.owlapi.model.OWLDataExactCardinality;
+import org.semanticweb.owlapi.model.OWLDataHasValue;
+import org.semanticweb.owlapi.model.OWLDataMaxCardinality;
+import org.semanticweb.owlapi.model.OWLDataMinCardinality;
+import org.semanticweb.owlapi.model.OWLDataSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLObjectAllValuesFrom;
+import org.semanticweb.owlapi.model.OWLObjectComplementOf;
+import org.semanticweb.owlapi.model.OWLObjectExactCardinality;
+import org.semanticweb.owlapi.model.OWLObjectHasSelf;
+import org.semanticweb.owlapi.model.OWLObjectHasValue;
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
+import org.semanticweb.owlapi.model.OWLObjectMaxCardinality;
+import org.semanticweb.owlapi.model.OWLObjectMinCardinality;
+import org.semanticweb.owlapi.model.OWLObjectOneOf;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLObjectUnionOf;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
@@ -59,20 +79,25 @@ public class FilteredOWLObjectPropertyHierarchyProvider extends OWLObjectPropert
 	}
 	
 	private boolean isSubClass(OWLOntology ont, OWLClass sub, OWLClass sup) {
-		if (sub.equals(sup)) {
-			return true;
-		}
+				
+		CheckSubsumptionRelation checker = new CheckSubsumptionRelation(ont, sup);
 		
 		Set<OWLSubClassOfAxiom> subaxs = ont.getSubClassAxiomsForSubClass(sub);
 		for (OWLSubClassOfAxiom s : subaxs) {
-			if (!s.getSuperClass().isAnonymous()) {
-				if (s.getSuperClass().asOWLClass().equals(sup)) {
-					return true;
+			s.getSuperClass().accept(checker);
+			if (checker.isSub()) {
+				return true;
+			}
+		}
+		
+		Set<OWLEquivalentClassesAxiom> eqs = ont.getEquivalentClassesAxioms(sub);
+		for (OWLEquivalentClassesAxiom eq : eqs) {
+			for (OWLClassExpression exp : eq.getClassExpressions()) {
+				if (!exp.isAnonymous() && exp.asOWLClass().equals(sub)) {
 				} else {
-					if (sub.equals(s.getSuperClass().asOWLClass())) {
-						return false;
-					} else {
-						return isSubClass(ont, s.getSuperClass().asOWLClass(), sup);
+					exp.accept(checker);
+					if (checker.isSub()) {
+						return true;
 					}
 				}
 			}
@@ -97,6 +122,138 @@ public class FilteredOWLObjectPropertyHierarchyProvider extends OWLObjectPropert
 			
 		}
 		return null;		
+	}
+	
+	private class CheckSubsumptionRelation implements OWLClassExpressionVisitor {
+		
+		private boolean isSub = false;
+		
+		public boolean isSub() { return isSub; }
+		
+		private OWLClass sup = null;
+		
+		private OWLOntology ont = null;
+		
+		public CheckSubsumptionRelation(OWLOntology o, OWLClass c) {
+			sup = c;
+			ont = o;
+		}
+
+		@Override
+		public void visit(OWLClass ce) {
+			if (ce.equals(sup)) {
+				isSub = true;
+			} else {
+				isSub = isSubClass(ont, ce, sup);
+			}
+		}
+
+		@Override
+		public void visit(OWLObjectIntersectionOf ce) {
+			Set<OWLClassExpression> cjs = ce.asConjunctSet();
+			for (OWLClassExpression exp : cjs) {
+				exp.accept(this);
+			}
+		}
+
+		@Override
+		public void visit(OWLObjectUnionOf ce) {
+			Set<OWLClassExpression> cjs = ce.asDisjunctSet();
+			for (OWLClassExpression exp : cjs) {
+				exp.accept(this);
+			}
+		}
+
+		@Override
+		public void visit(OWLObjectComplementOf ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectSomeValuesFrom ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectAllValuesFrom ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectHasValue ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectMinCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectExactCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectMaxCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectHasSelf ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLObjectOneOf ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataSomeValuesFrom ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataAllValuesFrom ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataHasValue ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataMinCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataExactCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visit(OWLDataMaxCardinality ce) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 }
