a99aad173b24cb4e3f1486efc412ab85ebe85d90
Matthew Horridge <matthew.horridge@stanford.edu>
2017-09-08 09:42:16 -0700

    Created a "hamburger" menu button

---
+++ protege-editor-core/src/main/java/org/protege/editor/core/ui/menu/MenuButton.java
@@ -0,0 +1,24 @@
+package org.protege.editor.core.ui.menu;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Cursor.HAND_CURSOR;
+import static java.awt.Cursor.getPredefinedCursor;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 8 Sep 2017
+ */
+public class MenuButton extends JButton {
+
+    public MenuButton() {
+        super(MenuIcon.getGrayIcon());
+        setBackground(null);
+        setBorder(null);
+        setCursor(getPredefinedCursor(HAND_CURSOR));
+        setRolloverEnabled(true);
+        setRolloverIcon(MenuIcon.getDarkGrayIcon());
+    }
+}
