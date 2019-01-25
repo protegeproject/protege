d90d83df49d6c58ff2390a0fc846f7367c9b7149
Matthew Horridge <matthew.horridge@stanford.edu>
2019-01-25 09:30:26 -0800

    Closes #841

--- protege-editor-owl/src/main/java/org/protege/editor/owl/ui/view/EntityBannerFormatterImpl.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/view/EntityBannerFormatterImpl.java
@@ -1,6 +1,7 @@
 package org.protege.editor.owl.ui.view;
 
 import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.renderer.RenderingEscapeUtils;
 import org.semanticweb.owlapi.model.OWLEntity;
 
 import javax.annotation.Nonnull;
@@ -20,6 +21,7 @@ public class EntityBannerFormatterImpl implements EntityBannerFormatter {
     @Override
     public String formatBanner(@Nonnull OWLEntity entity, @Nonnull OWLEditorKit editorKit) {
         String rendering = editorKit.getOWLModelManager().getRendering(entity);
+        String unescapedRendering = RenderingEscapeUtils.unescape(rendering);
         String iri = entity.getIRI().toString();
         String oboStyleId = "";
         Matcher oboIdMatcher = OBO_STYLE_ID_PATTERN.matcher(iri);
@@ -28,7 +30,7 @@ public class EntityBannerFormatterImpl implements EntityBannerFormatter {
         }
         return String.format(
                 "<html><body><nobr>%s <span style='color: #808080;'>%s \u2014 %s</span><nobr></body></html>",
-                rendering,
+                unescapedRendering,
                 oboStyleId,
                 iri);
     }
