01adadc07ef099a7412cf83d7aa8e50de7d6ad15
Matthew Horridge <matthew.horridge@stanford.edu>
2019-04-05 14:58:59 -0700

    Added merge strategy

---
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/merge/MergeStrategy.java
@@ -0,0 +1,13 @@
+package org.protege.editor.owl.ui.merge;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-04-05
+ */
+public enum MergeStrategy {
+
+    DELETE_SOURCE_ENTITY,
+
+    DEPRECATE_TARGET_ENTITY
+}
