fd89c2d77923b4f916acdfe404c5900970ed8463
Matthew Horridge <matthew.horridge@stanford.edu>
2019-04-15 10:34:38 -0700

    Implements #861

---
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/model/util/MemoryMonitor.java
@@ -0,0 +1,75 @@
+package org.protege.editor.owl.model.util;
+
+import javax.annotation.Nonnull;
+import javax.swing.*;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-04-12
+ */
+public class MemoryMonitor {
+
+    public enum MemoryStatus {
+        MEMORY_LOW, MEMORY_OK
+    }
+
+    private static final int LOW_MEMORY_THRESHOLD_BYTES = 200_048_576;
+
+    @Nonnull
+    private final LowMemoryNotificationView view;
+
+    private boolean displayedLowMemoryNotification;
+
+    public MemoryMonitor(@Nonnull LowMemoryNotificationView view) {
+        this.view = checkNotNull(view);
+    }
+
+    /**
+     * Checks the memory availability and displays a warning message if memory is low
+     */
+    public MemoryStatus checkMemory() {
+        long freeMem = getFreeMemoryInBytes();
+        if(freeMem < LOW_MEMORY_THRESHOLD_BYTES) {
+            displayLowMemoryNotification();
+            return MemoryStatus.MEMORY_LOW;
+        }
+        else {
+            return MemoryStatus.MEMORY_OK;
+        }
+
+    }
+
+    private static long getFreeMemoryInBytes() {
+        Runtime r = Runtime.getRuntime();
+        return r.maxMemory() - r.totalMemory();
+    }
+
+    private synchronized void resumeThreads() {
+        notifyAll();
+    }
+
+    private synchronized void displayLowMemoryNotification() {
+        if(!displayedLowMemoryNotification) {
+            displayedLowMemoryNotification = true;
+            SwingUtilities.invokeLater(() -> {
+                view.displayLowMemoryNotification();
+                resumeThreads();
+            });
+            pauseCurrentThreadIfNotEdt();
+        }
+    }
+
+    private void pauseCurrentThreadIfNotEdt() {
+        if(SwingUtilities.isEventDispatchThread()) {
+            return;
+        }
+        try {
+            wait();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/ui/util/LowMemoryNotificationViewImpl.java
@@ -0,0 +1,46 @@
+package org.protege.editor.owl.ui.util;
+
+import org.protege.editor.owl.model.util.LowMemoryNotificationView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.stream.Stream;
+
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-04-12
+ */
+public class LowMemoryNotificationViewImpl implements LowMemoryNotificationView {
+
+    private static final String TITLE = "Low Memory";
+
+    private static final String MSG = "<html><body><b>The memory available to Protégé is running low<br><br>Free memory: %d MB</b>&nbsp;&nbsp;&nbsp;&nbsp;(Memory limit: %d MB)<br><br>This may cause Protégé to freeze and become unresponsive</body></html>";
+
+    public static void main(String[] args) {
+        LowMemoryNotificationViewImpl i = new LowMemoryNotificationViewImpl();
+        i.displayLowMemoryNotification();
+    }
+
+    /**
+     * Display a low memory notification.  This method will only be called
+     * in the Event Dispatch Thread.
+     */
+    @Override
+    public void displayLowMemoryNotification() {
+        Window parent = Stream
+                .of(Window.getOwnerlessWindows())
+                .filter(w -> w instanceof JFrame)
+                .findFirst()
+                .orElse(null);
+        Runtime runtime = Runtime.getRuntime();
+        long availableMemory = runtime.maxMemory() - runtime.totalMemory();
+        long availableMemoryInMb = availableMemory / 1024 / 1024;
+        long maxMemory = runtime.maxMemory();
+        long maxMemoryInMb = maxMemory / 1024 / 1024;
+        String formattedMsg = String.format(MSG, availableMemoryInMb, maxMemoryInMb);
+        JOptionPane.showMessageDialog(parent, formattedMsg, TITLE, WARNING_MESSAGE);
+    }
+}
