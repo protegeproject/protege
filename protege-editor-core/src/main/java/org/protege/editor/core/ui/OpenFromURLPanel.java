5aa6327e4fe354d6684aa93c15eb082f449c4a14
Matthew Horridge <matthew.horridge@stanford.edu>
2019-04-15 16:00:06 -0700

    Tidied up Open from URL dialog

--- protege-editor-core/src/main/java/org/protege/editor/core/ui/OpenFromURLPanel.java
+++ protege-editor-core/src/main/java/org/protege/editor/core/ui/OpenFromURLPanel.java
@@ -4,23 +4,21 @@ import org.protege.editor.core.BookMarkedURIManager;
 import org.protege.editor.core.ui.list.MList;
 import org.protege.editor.core.ui.list.MListItem;
 import org.protege.editor.core.ui.list.MListSectionHeader;
-import org.protege.editor.core.ui.util.ComponentFactory;
-import org.protege.editor.core.ui.util.InputVerificationStatusChangedListener;
-import org.protege.editor.core.ui.util.JOptionPaneEx;
-import org.protege.editor.core.ui.util.VerifiedInputEditor;
+import org.protege.editor.core.ui.util.*;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static javax.swing.JOptionPane.*;
 
 
 /**
@@ -30,6 +28,18 @@ import java.util.TreeSet;
  * Date: 12-May-2007<br><br>
  */
 public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
+
+    private static final int PREF_WIDTH = 500;
+
+    private static final int PREF_HEIGHT = 300;
+
+    private static final String TITLE = "Enter or select a URL";
+
+    private static final String URL_FIELD_PLACEHOLDER = "Enter URL to open from";
+
+    private static final String URL_FIELD_LABEL = "URL";
+
+    private static final String BOOKMARKED_URLS_LABEL = "Bookmarked URLs";
 
     private JTextField uriField;
 
@@ -45,8 +55,7 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
 
 
     private void createUI() {
-        setLayout(new BorderLayout());
-        uriField = new JTextField(45);
+        uriField = new AugmentedJTextField("", 45, URL_FIELD_PLACEHOLDER);
         uriField.getDocument().addDocumentListener(new DocumentListener(){
             public void insertUpdate(DocumentEvent event) {
                 handleValueChanged();
@@ -58,34 +67,34 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
                 handleValueChanged();
             }
         });
-        JPanel uriFieldHolder = new JPanel(new BorderLayout());
-        uriFieldHolder.setBorder(ComponentFactory.createTitledBorder("URI"));
-        uriFieldHolder.add(uriField, BorderLayout.NORTH);
-        add(uriFieldHolder, BorderLayout.NORTH);
-        JPanel bookmarksHolder = new JPanel(new BorderLayout());
-        bookmarksHolder.setBorder(ComponentFactory.createTitledBorder("Bookmarks"));
-        add(bookmarksHolder);
+
+        JPanel upperGroup = new JPanel(new BorderLayout());
+        upperGroup.add(new FormLabel(URL_FIELD_LABEL), BorderLayout.NORTH);
+        upperGroup.add(uriField, BorderLayout.SOUTH);
+
+        JPanel lowerGroup = new JPanel(new BorderLayout());
+        lowerGroup.add(new FormLabel(BOOKMARKED_URLS_LABEL), BorderLayout.NORTH);
         bookmarksList = new MList() {
-
-
             protected void handleAdd() {
                 addURI();
             }
-
-
             protected void handleDelete() {
                 deleteSelectedBookmark();
             }
         };
-
         bookmarksList.setCellRenderer(new BookmarkedItemListRenderer());
-        bookmarksHolder.add(new JScrollPane(bookmarksList));
+        JScrollPane scrollPane = new JScrollPane(bookmarksList);
+        lowerGroup.add(scrollPane);
         fillList();
         bookmarksList.addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
                 updateTextField();
             }
         });
+        setLayout(new BorderLayout(7, 7));
+        add(upperGroup, BorderLayout.NORTH);
+        add(lowerGroup, BorderLayout.CENTER);
+        setPreferredSize(new Dimension(PREF_WIDTH, PREF_HEIGHT));
     }
 
 
@@ -118,7 +127,7 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
     }
 
     private void updateTextField() {
-        URIListItem item = getSelUriListItem();
+        UrlListItem item = getSelUriListItem();
         if (item != null) {
             uriField.setText(item.uri.toString());
         }
@@ -126,7 +135,7 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
 
 
     private void addURI() {
-        String uriString = JOptionPane.showInputDialog(this, "Please enter a URI", "URI", JOptionPane.PLAIN_MESSAGE);
+        String uriString = JOptionPane.showInputDialog(this, "Please enter a URL", "URL", PLAIN_MESSAGE);
         if (uriString != null) {
             try {
                 URI uri = new URI(uriString);
@@ -141,38 +150,40 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
 
 
     private void showURIErrorMessage(URISyntaxException e) {
-        JOptionPane.showMessageDialog(this, e.getMessage(), "Invalid URI", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(this, e.getMessage(), "Invalid URL", ERROR_MESSAGE);
     }
 
 
     private void fillList() {
-        BookMarkedURIManager man = BookMarkedURIManager.getInstance();
-        Set<URI> ts = new TreeSet<>();
-        ts.addAll(man.getBookMarkedURIs());
-        ArrayList<Object> data = new ArrayList<>();
+        List<Object> listData = new ArrayList<>();
+        listData.add(new AddUrlItem());
+        getBookmarkedUrls()
+                .sorted()
+                .map(UrlListItem::new)
+                .forEach(listData::add);
+        bookmarksList.setListData(listData.toArray());
+    }
 
-        data.add(new AddURIItem());
-        for (URI uri : ts) {
-            data.add(new URIListItem(uri));
-        }
-        bookmarksList.setListData(data.toArray());
+    private static Stream<URI> getBookmarkedUrls() {
+        BookMarkedURIManager man = BookMarkedURIManager.getInstance();
+        return man.getBookMarkedURIs().stream();
     }
 
 
     private void deleteSelectedBookmark() {
         Object selObj = bookmarksList.getSelectedValue();
-        if (!(selObj instanceof URIListItem)) {
+        if (!(selObj instanceof UrlListItem)) {
             return;
         }
-        URIListItem item = (URIListItem) selObj;
+        UrlListItem item = (UrlListItem) selObj;
         BookMarkedURIManager.getInstance().remove(item.uri);
         fillList();
     }
 
 
-    private URIListItem getSelUriListItem() {
-        if (bookmarksList.getSelectedValue() instanceof URIListItem) {
-            return (URIListItem) bookmarksList.getSelectedValue();
+    private UrlListItem getSelUriListItem() {
+        if (bookmarksList.getSelectedValue() instanceof UrlListItem) {
+            return (UrlListItem) bookmarksList.getSelectedValue();
         }
         return null;
     }
@@ -189,30 +200,26 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
     }
 
 
-    private class BookmarkedItemListRenderer extends DefaultListCellRenderer {
-
-        /**
-         * 
-         */
-        private static final long serialVersionUID = -833970269120392171L;
+    private static class BookmarkedItemListRenderer extends DefaultListCellRenderer {
 
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
                                                       boolean cellHasFocus) {
             JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
-            if (value instanceof URIListItem) {
-                URIListItem item = (URIListItem) value;
+            if (value instanceof UrlListItem) {
+                UrlListItem item = (UrlListItem) value;
                 label.setText(item.uri.toString());
             }
+            label.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
             return label;
         }
     }
 
 
-    private class AddURIItem implements MListSectionHeader {
+    private static class AddUrlItem implements MListSectionHeader {
 
         public String getName() {
-            return "Bookmarked URIs";
+            return "Bookmarked URLs";
         }
 
 
@@ -222,34 +229,27 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
     }
 
 
-    private class URIListItem implements MListItem {
+    private static class UrlListItem implements MListItem {
 
-        private URI uri;
+        private final URI uri;
 
-
-        public URIListItem(URI uri) {
+        public UrlListItem(URI uri) {
             this.uri = uri;
         }
-
 
         public boolean isEditable() {
             return false;
         }
 
-
-        public void handleEdit() {
-        }
-
+        public void handleEdit() {}
 
         public boolean isDeleteable() {
             return true;
         }
 
-
         public boolean handleDelete() {
             return true;
         }
-
 
         public String getTooltip() {
             return uri.toString();
@@ -259,15 +259,18 @@ public class OpenFromURLPanel extends JPanel implements VerifiedInputEditor {
 
     public static URI showDialog() {
         OpenFromURLPanel panel = new OpenFromURLPanel();
-        int ret = JOptionPaneEx.showValidatingConfirmDialog(null,
-                                                  "Enter or select a URI",
-                                                  panel,
-                                                  JOptionPane.PLAIN_MESSAGE,
-                                                  JOptionPane.OK_CANCEL_OPTION,
-                                                  panel.uriField);
+        int ret = JOptionPaneEx.showValidatingConfirmDialog(null, TITLE,
+                                                            panel,
+                                                            PLAIN_MESSAGE,
+                                                            OK_CANCEL_OPTION,
+                                                            panel.uriField);
         if (ret == JOptionPane.OK_OPTION) {
             return panel.getURI();
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        OpenFromURLPanel.showDialog();
     }
 }
