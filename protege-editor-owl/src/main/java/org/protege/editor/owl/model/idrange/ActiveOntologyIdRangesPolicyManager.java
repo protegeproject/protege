c875989413f30b0de80333c1e86bd7994d49b4cc
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2023-10-31 23:37:35 +0000

    Allow to ignore ID range policy files.
    Add an option to the Entity Creation Preferences panel to disable
    completely the ID range policy feature. When that option is enabled,
    Protege will ignore any -idranges.owl file.

--- protege-editor-owl/src/main/java/org/protege/editor/owl/model/idrange/ActiveOntologyIdRangesPolicyManager.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/model/idrange/ActiveOntologyIdRangesPolicyManager.java
@@ -60,6 +60,9 @@ public class ActiveOntologyIdRangesPolicyManager {
 
     public void reload() {
         EntityCreationPreferences.setPolicyRangeName(null);
+        if (EntityCreationPreferences.getIgnoreRangePolicy()) {
+            return;
+        }
         idRangesPolicies = ImmutableList.of();
         OWLOntology activeOntology = modelManager.getActiveOntology();
         URI activeOntologyLocation = modelManager.getOntologyPhysicalURI(activeOntology);
