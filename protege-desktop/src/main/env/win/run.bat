579de1628fb1c9ab00804d38b49d1d09ea42d873
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-09-19 15:57:12 +0800

    release on 5.6.10.1

--- protege-desktop/src/main/env/win/run.bat
+++ protege-desktop/src/main/env/win/run.bat
@@ -1,3 +1,20 @@
+@echo off
 setlocal
 cd /d %~dp0
-jre\bin\java ${conf.extra.args} -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback-win.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bundles/guava.jar;bundles/logback-classic.jar;bundles/logback-core.jar;bundles/slf4j-api.jar;bundles/glassfish-corba-orb.jar;bundles/org.apache.felix.main.jar;bundles/maven-artifact.jar;bundles/protege-launcher.jar org.protege.osgi.framework.Launcher %1
+
+jre\bin\java ^
+  --add-opens java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED ^
+  --add-opens java.xml/com.sun.org.apache.xml.internal.serialize=ALL-UNNAMED ^
+  --add-opens java.base/java.lang=ALL-UNNAMED ^
+  --add-opens java.base/java.net=ALL-UNNAMED ^
+  --add-opens java.base/java.util=ALL-UNNAMED ^
+  --add-opens java.base/sun.net.www=ALL-UNNAMED ^
+  --add-opens java.base/jdk.internal.loader=ALL-UNNAMED ^
+  --add-exports java.base/sun.security.action=ALL-UNNAMED ^
+  -XX:CompileCommand=exclude,javax/swing/text/GlyphView,getBreakSpot ^
+  -DentityExpansionLimit=100000000 ^
+  -Dlogback.configurationFile=conf/logback-win.xml ^
+  -Dfile.encoding=utf-8 ^
+  -Dorg.protege.plugin.dir=plugins ^
+  -classpath "lib/org.eclipse.osgi.jar;bundles/guava.jar;bundles/logback-classic.jar;bundles/logback-core.jar;bundles/slf4j-api.jar;bundles/glassfish-corba-orb.jar;bundles/org.apache.felix.main.jar;bundles/maven-artifact.jar;bundles/protege-launcher.jar" ^
+  org.protege.osgi.framework.Launcher %1
