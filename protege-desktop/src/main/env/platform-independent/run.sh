7dc3fa47fe2891a3a99b6a54035c8c26d00cc49b
Matthew Horridge <matthew.horridge@stanford.edu>
2026-09-24 10:17:11 -0700

    Remove CORBA runtime from Protégé 6
    - remove the GlassFish CORBA dependency from the launcher
    - remove CORBA artifacts from the distribution assemblies
    - remove the CORBA JAR from the application classpaths
    
    Closes #1379

--- protege-desktop/src/main/env/platform-independent/run.sh
+++ protege-desktop/src/main/env/platform-independent/run.sh
@@ -32,7 +32,7 @@ $EFFECTIVE_JAVA_HOME/bin/java \
      -Dfile.encoding=UTF-8 \
      ${conf.extra.args} \
      --add-opens=java.desktop/sun.swing=ALL-UNNAMED \
-     -classpath bundles/guava.jar:bundles/logback-classic.jar:bundles/logback-core.jar:bundles/slf4j-api.jar:bundles/glassfish-corba-orb.jar:bundles/org.apache.felix.main.jar:bundles/maven-artifact.jar:bundles/protege-launcher.jar \
+     -classpath bundles/guava.jar:bundles/logback-classic.jar:bundles/logback-core.jar:bundles/slf4j-api.jar:bundles/org.apache.felix.main.jar:bundles/maven-artifact.jar:bundles/protege-launcher.jar \
      $CMD_OPTIONS $EXTRA_JVM_OPTIONS org.protege.osgi.framework.Launcher $1
 
 
